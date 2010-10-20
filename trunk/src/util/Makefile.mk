f6052bfd175dd3fa276066cabdb19cdfc74f0f26
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2010-10-20 16:49:59 +0000

    rename
    git-svn-id: https://svn.mcs.anl.gov/repos/ospri@672 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- trunk/src/util/Makefile.mk
+++ trunk/src/util/Makefile.mk
@@ -3,8 +3,9 @@
 #     See COPYRIGHT in top-level directory.
 #
 
-libosp_la_SOURCES += $(top_srcdir)/src/adl/util/ospu_put.c \
-                    $(top_srcdir)/src/adl/util/ospu_get.c \
-                    $(top_srcdir)/src/adl/util/ospu_acc.c \
-                    $(top_srcdir)/src/adl/util/ospu_mod.c \
-                    $(top_srcdir)/src/adl/util/ospu_param.c 
+libosp_la_SOURCES += $(top_srcdir)/src/util/ospu_put.c \
+                    $(top_srcdir)/src/util/ospu_get.c \
+                    $(top_srcdir)/src/util/ospu_acc.c \
+                    $(top_srcdir)/src/util/ospu_mod.c \
+                    $(top_srcdir)/src/util/ospu_timer.c \
+                    $(top_srcdir)/src/util/ospu_param.c 
