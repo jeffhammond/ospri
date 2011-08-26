3aa2668569960b114748ce1cdbeb6689f2bab91b
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2011-08-26 12:50:51 +0000

    compiler macros (obviously device independent)
    git-svn-id: https://svn.mcs.anl.gov/repos/ospri@702 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- trunk/src/devices/common/compilers.h
+++ trunk/src/devices/common/compilers.h
@@ -6,3 +6,12 @@
 #define likely_if(x) if(__builtin_expect(x,1))
 #define unlikely_if(x) if(__builtin_expect(x,0))
 
+/* Dave Goodell's version in ARMCI-MPI
+#if defined(__GNUC__) && (__GNUC__ >= 3)
+#  define unlikely(x_) __builtin_expect(!!(x_),0)
+#  define likely(x_)   __builtin_expect(!!(x_),1)
+#else
+#  define unlikely(x_) (x_)
+#  define likely(x_)   (x_)
+#endif
+*/
