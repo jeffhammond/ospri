ed2bf6e509529fa38b52a1e3f5069159408413da
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2011-08-31 18:40:56 +0000

    git-svn-id: https://svn.mcs.anl.gov/repos/ospri@706 4a0c8edc-5189-0410-a5da-cd986011a6a8

---
+++ trunk/src/devices/dcmfd/Makefile.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2010 by Argonne National Laboratory.
+#     See COPYRIGHT in top-level directory.
+#
+
+libosp_la_SOURCES += $(top_srcdir)/src/devices/dcmfd/dcmfd_callbacks.c \
+                     $(top_srcdir)/src/devices/dcmfd/dcmfd_misc.c \
+                     $(top_srcdir)/src/devices/dcmfd/dcmfd_initialize.c \
+                     $(top_srcdir)/src/devices/dcmfd/dcmfd_finalize.c \
+                     $(top_srcdir)/src/devices/dcmfd/dcmfd_put.c \
+                     $(top_srcdir)/src/devices/dcmfd/dcmfd_get.c \
+
