6ee200d7d85bcd14385b612b661904e45285a1c9
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2011-09-20 17:40:53 +0000

    starting to test pragmas
    git-svn-id: https://svn.mcs.anl.gov/repos/ospri@738 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- branches/transpose/transpose.c
+++ branches/transpose/transpose.c
@@ -31,10 +31,15 @@ int main(int argc, char* argv[])
         unsigned long long * d2 = safemalloc( max * sizeof(unsigned long long) );
         unsigned long long * d3 = safemalloc( max * sizeof(unsigned long long) );
         unsigned long long * d4 = safemalloc( max * sizeof(unsigned long long) );
+        unsigned long long * d5 = safemalloc( max * sizeof(unsigned long long) );
 
+        /*           12345678901234567890123456789012 */
         char * n0 = "reference - direct copy";
         char * n1 = "basic w/ stride-1 stores";
         char * n2 = "basic w/ stride-1 loads";
+        char * n3 = "pragma unroll 4x4 + s1 stores";
+        char * n4 = "pragma unroll 4x4 + s1 loads";
+        char * n5 = "";
 
         for ( int n=2 ; n<max ; n++)
         {
@@ -45,8 +50,8 @@ int main(int argc, char* argv[])
      
             unsigned long long t0, t1;
 
-            A = malloc( N * sizeof(double) ); assert( A != NULL );
-            B = malloc( N * sizeof(double) ); assert( B != NULL );
+            A = safemalloc( N * sizeof(double) );
+            B = safemalloc( N * sizeof(double) );
      
             for ( int i=0 ; i<N ; i++ ) A[i] = (double)i;
             for ( int i=0 ; i<N ; i++ ) B[i] = 0.0;
@@ -54,29 +59,61 @@ int main(int argc, char* argv[])
             /* reference - direct copy */
             t0 = getticks();        
             for ( int t=0 ; t<16 ; t++ )
+            {
                 for ( int i=0 ; i<n ; i++ )
                     for ( int j=0 ; j<n ; j++ )
                         B[i*n+j] = A[i*n+j];
+            }
             t1 = getticks();        
             d0[n] = (t1-t0)/16;
 
             /* basic w/ stride-1 stores */
             t0 = getticks();        
             for ( int t=0 ; t<16 ; t++ )
+            {
                 for ( int i=0 ; i<n ; i++ )
                     for ( int j=0 ; j<n ; j++ )
                         B[i*n+j] = A[j*n+i];
+            }
             t1 = getticks();        
             d1[n] = (t1-t0)/16;
      
             /* basic w/ stride-1 loads */
             t0 = getticks();        
             for ( int t=0 ; t<16 ; t++ )
+            {
                 for ( int j=0 ; j<n ; j++ )
                     for ( int i=0 ; i<n ; i++ )
                         B[i*n+j] = A[j*n+i];
+            }
             t1 = getticks();        
             d2[n] = (t1-t0)/16;
+     
+            /* pragma unroll 4x4 + s1 stores */
+            t0 = getticks();        
+            for ( int t=0 ; t<16 ; t++ )
+            {
+                #pragma unroll(4)
+                for ( int i=0 ; i<n ; i++ )
+                    #pragma unroll(4)
+                    for ( int j=0 ; j<n ; j++ )
+                        B[i*n+j] = A[j*n+i];
+            }
+            t1 = getticks();        
+            d3[n] = (t1-t0)/16;
+     
+            /* pragma unroll 4x4 + s1 loads */
+            t0 = getticks();        
+            for ( int t=0 ; t<16 ; t++ )
+            {
+                #pragma unroll(4)
+                for ( int j=0 ; j<n ; j++ )
+                    #pragma unroll(4)
+                    for ( int i=0 ; i<n ; i++ )
+                        B[i*n+j] = A[j*n+i];
+            }
+            t1 = getticks();        
+            d4[n] = (t1-t0)/16;
      
             free(B);
             free(A);
@@ -91,6 +128,12 @@ int main(int argc, char* argv[])
         }
         fflush( stdout );
 
+        free(d5);
+        free(d4);
+        free(d3);
+        free(d2);
+        free(d1);
+        free(d0);
     }
  
     rc = MPI_Finalize();
