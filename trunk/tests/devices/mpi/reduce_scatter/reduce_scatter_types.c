4248da44b582a215aa0c0e26fdf751938d596ff6
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2011-09-23 14:45:08 +0000

    halfway there
    git-svn-id: https://svn.mcs.anl.gov/repos/ospri@775 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- trunk/tests/devices/mpi/reduce_scatter/reduce_scatter_types.c
+++ trunk/tests/devices/mpi/reduce_scatter/reduce_scatter_types.c
@@ -11,62 +11,12 @@
 #  include <common/bgp_personality_inlines.h>
 #endif
 
-int main(int argc, char *argv[])
+int size, rank;
+int rc;
+int count, min_count, max_count;
+
+void test_sendrecv(int rank, int size, MPI_Datatype type, MPI_Op op)
 {
-    int provided;
-    int size, rank;
-    int rc;
-
-    MPI_Init_thread( &argc, &argv, MPI_THREAD_SINGLE, &provided );
-    assert( provided == MPI_THREAD_SINGLE );
-
-    MPI_Comm_size( MPI_COMM_WORLD, &size );
-    MPI_Comm_rank( MPI_COMM_WORLD, &rank );
-
-    //printf( "Hello from %d of %d processors\n", rank, size );
-    //fflush( stdout );
-
-    int count;
-    int min_count = (argc > 1 ? atoi(argv[1]) : 1);
-    int max_count = (argc > 2 ? atoi(argv[2]) : 1024);
-
-    if ( rank == 0 ) printf( "world size=%d min_count=%d max_count=%d\n", size , min_count , max_count );
-#ifdef __bgp__
-    {
-        _BGP_Personality_t pers;
-        char* mode = "WTF ";
-        char* torusX = "?";
-        char* torusY = "?";
-        char* torusZ = "?";
-        int sizeX = -1;
-        int sizeY = -1;
-        int sizeZ = -1;
-
-        if (rank == 0)
-        {
-             Kernel_GetPersonality(&pers, sizeof(pers));
-          
-                  if ( BGP_Personality_processConfig(&pers) == _BGP_PERS_PROCESSCONFIG_SMP ) mode = "SMP ";
-             else if ( BGP_Personality_processConfig(&pers) == _BGP_PERS_PROCESSCONFIG_VNM ) mode = "VNM ";
-             else if ( BGP_Personality_processConfig(&pers) == _BGP_PERS_PROCESSCONFIG_2x2 ) mode = "DUAL";
-          
-             torusX = BGP_Personality_isTorusX(&pers) ? "T" : "F";
-             torusY = BGP_Personality_isTorusY(&pers) ? "T" : "F";
-             torusZ = BGP_Personality_isTorusZ(&pers) ? "T" : "F";
-          
-             sizeX = BGP_Personality_xSize(&pers);
-             sizeY = BGP_Personality_ySize(&pers);
-             sizeZ = BGP_Personality_zSize(&pers);
-          
-             printf("mode %s topology (%2d,%2d,%2d) torus? (%1s,%1s,%1s)\n",
-                     mode, sizeX, sizeY, sizeZ, torusX, torusY, torusZ );
-        }
-    }
-#endif
-    fflush( stdout );
-
-    MPI_Barrier( MPI_COMM_WORLD );
-
     /* send-recv bandwidth test */
     if ( rank == 0 ) printf( "begin send-recv bandwidth test\n" );
     if ( size > 1 )
@@ -92,13 +42,13 @@ int main(int argc, char *argv[])
 
         if ( rank == dst_rank )
         {
-             rc = MPI_Recv( buffer, count, MPI_INT, src_rank, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE );
+             rc = MPI_Recv( buffer, count, type, src_rank, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE );
              assert ( rc == MPI_SUCCESS );
         }
         else if ( rank == src_rank ) 
         {
              t0 = MPI_Wtime();
-             rc = MPI_Send( buffer, count, MPI_INT, dst_rank, tag, MPI_COMM_WORLD );
+             rc = MPI_Send( buffer, count, type, dst_rank, tag, MPI_COMM_WORLD );
              t1 = MPI_Wtime();
              assert ( rc == MPI_SUCCESS );
         }
@@ -118,7 +68,10 @@ int main(int argc, char *argv[])
 
         free(buffer);
     }
+}
 
+void test_reduce(int rank, int size, MPI_Datatype type, MPI_Op op)
+{
     /* reduce bandwidth test */
     if ( rank == 0 ) printf( "begin reduce bandwidth test\n" );
     for ( count = min_count; count < max_count ; count *= 2 )
@@ -137,8 +90,8 @@ int main(int argc, char *argv[])
         MPI_Barrier( MPI_COMM_WORLD );
 
         t0 = MPI_Wtime();
-        if ( rank == root ) rc = MPI_Reduce( MPI_IN_PLACE, buffer, size * count, MPI_INT, MPI_SUM, root, MPI_COMM_WORLD );
-        else                rc = MPI_Reduce( buffer      , NULL  , size * count, MPI_INT, MPI_SUM, root, MPI_COMM_WORLD );
+        if ( rank == root ) rc = MPI_Reduce( MPI_IN_PLACE, buffer, size * count, type, op, root, MPI_COMM_WORLD );
+        else                rc = MPI_Reduce( buffer      , NULL  , size * count, type, op, root, MPI_COMM_WORLD );
         t1 = MPI_Wtime();
         assert ( rc == MPI_SUCCESS );
 
@@ -148,7 +101,7 @@ int main(int argc, char *argv[])
                     assert( buffer[ r * count + i ] == r );
 
         if ( rank == 0 ) printf( "%35s: %10u bytes (%7u bytes per rank) transferred in %6.2e seconds (%6.2e MB/s or %6.2e MB/s bytes per rank)\n",
-                                 "MPI_Reduce(MPI_SUM)",
+                                 "MPI_Reduce(op)",
                                  size * count * (int) sizeof(int), count * (int) sizeof(int), 
                                  t1 - t0, 
                                  1e-6 * size * count * sizeof(int) / (t1-t0),
@@ -156,7 +109,10 @@ int main(int argc, char *argv[])
 
         free(buffer);
     }
+}
 
+void test_allreduce(int rank, int size, MPI_Datatype type, MPI_Op op)
+{
     /* allreduce bandwidth test */
     if ( rank == 0 ) printf( "begin allreduce bandwidth test\n" );
     for ( count = min_count; count < max_count ; count *= 2 )
@@ -174,7 +130,7 @@ int main(int argc, char *argv[])
         MPI_Barrier( MPI_COMM_WORLD );
 
         t0 = MPI_Wtime();
-        rc = MPI_Allreduce( MPI_IN_PLACE, buffer, size * count, MPI_INT, MPI_SUM, MPI_COMM_WORLD );
+        rc = MPI_Allreduce( MPI_IN_PLACE, buffer, size * count, type, op, MPI_COMM_WORLD );
         t1 = MPI_Wtime();
         assert ( rc == MPI_SUCCESS );
 
@@ -183,7 +139,7 @@ int main(int argc, char *argv[])
                 assert( buffer[ r * count + i ] == r );
 
         if ( rank == 0 ) printf( "%35s: %10u bytes (%7u bytes per rank) transferred in %6.2e seconds (%6.2e MB/s or %6.2e MB/s bytes per rank)\n",
-                                 "MPI_Allreduce(MPI_SUM)",
+                                 "MPI_Allreduce(op)",
                                  size * count * (int) sizeof(int), count * (int) sizeof(int), 
                                  t1 - t0, 
                                  1e-6 * size * count * sizeof(int) / (t1-t0),
@@ -191,7 +147,10 @@ int main(int argc, char *argv[])
 
         free(buffer);
     }
+}
 
+void test_scatter(int rank, int size, MPI_Datatype type, MPI_Op op)
+{
     /* scatter bandwidth test */
     if ( rank == 0 ) printf( "begin scatter bandwidth test\n" );
     for ( count = min_count; count < max_count ; count *= 2 )
@@ -226,7 +185,7 @@ int main(int argc, char *argv[])
         MPI_Barrier( MPI_COMM_WORLD );
 
         t0 = MPI_Wtime();
-        rc = MPI_Scatter( snd_buffer, count, MPI_INT, rcv_buffer, count , MPI_INT, root, MPI_COMM_WORLD );
+        rc = MPI_Scatter( snd_buffer, count, type, rcv_buffer, count , type, root, MPI_COMM_WORLD );
         t1 = MPI_Wtime();
         assert ( rc == MPI_SUCCESS );
 
@@ -242,7 +201,10 @@ int main(int argc, char *argv[])
         free(snd_buffer);
         free(rcv_buffer);
     }
+}
 
+void test_scatterv(int rank, int size, MPI_Datatype type, MPI_Op op)
+{
     /* scatterv bandwidth test */
     if ( rank == 0 ) printf( "begin scatterv bandwidth test\n" );
     for ( count = min_count; count < max_count ; count *= 2 )
@@ -286,7 +248,7 @@ int main(int argc, char *argv[])
         MPI_Barrier( MPI_COMM_WORLD );
 
         t0 = MPI_Wtime();
-        rc = MPI_Scatterv( snd_buffer, counts, displs, MPI_INT, rcv_buffer, count , MPI_INT, root, MPI_COMM_WORLD );
+        rc = MPI_Scatterv( snd_buffer, counts, displs, type, rcv_buffer, count , type, root, MPI_COMM_WORLD );
         t1 = MPI_Wtime();
         assert ( rc == MPI_SUCCESS );
 
@@ -304,7 +266,10 @@ int main(int argc, char *argv[])
         free(displs);
         free(counts);
     }
+}
 
+void test_reduce_then_scatter(int rank, int size, MPI_Datatype type, MPI_Op op)
+{
     /* reduce+scatter bandwidth test */
     if ( rank == 0 ) printf( "begin reduce+scatter bandwidth test\n" );
     for ( count = min_count; count < max_count ; count *= 2 )
@@ -328,17 +293,17 @@ int main(int argc, char *argv[])
         MPI_Barrier( MPI_COMM_WORLD );
 
         t0 = MPI_Wtime();
-        if ( rank == root ) rc = MPI_Reduce( MPI_IN_PLACE, snd_buffer, size * count, MPI_INT, MPI_SUM, root, MPI_COMM_WORLD );
-        else                rc = MPI_Reduce( snd_buffer      , NULL  , size * count, MPI_INT, MPI_SUM, root, MPI_COMM_WORLD );
+        if ( rank == root ) rc = MPI_Reduce( MPI_IN_PLACE, snd_buffer, size * count, type, op, root, MPI_COMM_WORLD );
+        else                rc = MPI_Reduce( snd_buffer      , NULL  , size * count, type, op, root, MPI_COMM_WORLD );
         if ( rc == MPI_SUCCESS )
-                            rc = MPI_Scatter( snd_buffer, count, MPI_INT, rcv_buffer, count , MPI_INT, root, MPI_COMM_WORLD );
+                            rc = MPI_Scatter( snd_buffer, count, type, rcv_buffer, count , type, root, MPI_COMM_WORLD );
         t1 = MPI_Wtime();
         assert ( rc == MPI_SUCCESS );
 
         for ( i = 0 ; i < count ; i++ ) assert( rcv_buffer[i] == rank );
 
         if ( rank == 0 ) printf( "%35s: %10u bytes (%7u bytes per rank) transferred in %6.2e seconds (%6.2e MB/s or %6.2e MB/s bytes per rank)\n",
-                                 "MPI_Reduce(MPI_SUM)+Scatter",
+                                 "MPI_Reduce(op)+Scatter",
                                  size * count * (int) sizeof(int), count * (int) sizeof(int), 
                                  t1 - t0, 
                                  1e-6 * size * count * sizeof(int) / (t1-t0),
@@ -347,7 +312,10 @@ int main(int argc, char *argv[])
         free(snd_buffer);
         free(rcv_buffer);
     }
+}
 
+void test_reduce_then_scatterv(int rank, int size, MPI_Datatype type, MPI_Op op)
+{
     /* reduce+scatterv bandwidth test */
     if ( rank == 0 ) printf( "begin reduce+scatterv bandwidth test\n" );
     for ( count = min_count; count < max_count ; count *= 2 )
@@ -381,17 +349,17 @@ int main(int argc, char *argv[])
         MPI_Barrier( MPI_COMM_WORLD );
 
         t0 = MPI_Wtime();
-        if ( rank == root ) rc = MPI_Reduce( MPI_IN_PLACE, snd_buffer, size * count, MPI_INT, MPI_SUM, root, MPI_COMM_WORLD );
-        else                rc = MPI_Reduce( snd_buffer      , NULL  , size * count, MPI_INT, MPI_SUM, root, MPI_COMM_WORLD );
+        if ( rank == root ) rc = MPI_Reduce( MPI_IN_PLACE, snd_buffer, size * count, type, op, root, MPI_COMM_WORLD );
+        else                rc = MPI_Reduce( snd_buffer      , NULL  , size * count, type, op, root, MPI_COMM_WORLD );
         if ( rc == MPI_SUCCESS )
-                            rc = MPI_Scatterv( snd_buffer, counts, displs, MPI_INT, rcv_buffer, count , MPI_INT, root, MPI_COMM_WORLD );
+                            rc = MPI_Scatterv( snd_buffer, counts, displs, type, rcv_buffer, count , type, root, MPI_COMM_WORLD );
         t1 = MPI_Wtime();
         assert ( rc == MPI_SUCCESS );
 
         for ( i = 0 ; i < count ; i++ ) assert( rcv_buffer[i] == rank );
 
         if ( rank == 0 ) printf( "%35s: %10u bytes (%7u bytes per rank) transferred in %6.2e seconds (%6.2e MB/s or %6.2e MB/s bytes per rank)\n",
-                                 "MPI_Reduce(MPI_SUM)+Scatterv",
+                                 "MPI_Reduce(op)+Scatterv",
                                  size * count * (int) sizeof(int), count * (int) sizeof(int), 
                                  t1 - t0, 
                                  1e-6 * size * count * sizeof(int) / (t1-t0),
@@ -402,7 +370,10 @@ int main(int argc, char *argv[])
         free(counts);
         free(displs);
     }
+}
 
+void test_reduce_scatter(int rank, int size, MPI_Datatype type, MPI_Op op)
+{
     /* reduce_scatter bandwidth test */
     if ( rank == 0 ) printf( "begin reduce_scatter bandwidth test\n" );
     for ( count = min_count; count < max_count ; count *= 2 )
@@ -430,14 +401,14 @@ int main(int argc, char *argv[])
         MPI_Barrier( MPI_COMM_WORLD );
 
         t0 = MPI_Wtime();
-        rc = MPI_Reduce_scatter( snd_buffer, rcv_buffer, counts, MPI_INT, MPI_SUM, MPI_COMM_WORLD );
+        rc = MPI_Reduce_scatter( snd_buffer, rcv_buffer, counts, type, op, MPI_COMM_WORLD );
         t1 = MPI_Wtime();
         assert ( rc == MPI_SUCCESS );
 
         for ( i = 0 ; i < count ; i++ ) assert( rcv_buffer[i] == rank );
 
         if ( rank == 0 ) printf( "%35s: %10u bytes (%7u bytes per rank) transferred in %6.2e seconds (%6.2e MB/s or %6.2e MB/s bytes per rank)\n",
-                                 "MPI_Reduce_scatter(MPI_SUM)",
+                                 "MPI_Reduce_scatter(op)",
                                  size * count * (int) sizeof(int), count * (int) sizeof(int), 
                                  t1 - t0, 
                                  1e-6 * size * count * sizeof(int) / (t1-t0),
@@ -447,7 +418,10 @@ int main(int argc, char *argv[])
         free(rcv_buffer);
         free(counts);
     }
+}
 
+void test_reduce_scatter_block(int rank, int size, MPI_Datatype type, MPI_Op op)
+{
 #if ( MPI_VERSION == 2 && MPI_SUBVERSION == 2 ) || MPI_VERSION >= 3
     /* reduce_scatter_block bandwidth test */
     if ( rank == 0 ) printf( "begin reduce_scatter_block bandwidth test\n" );
@@ -471,14 +445,14 @@ int main(int argc, char *argv[])
         MPI_Barrier( MPI_COMM_WORLD );
 
         t0 = MPI_Wtime();
-        rc = MPI_Reduce_scatter_block( snd_buffer, rcv_buffer, count, MPI_INT, MPI_SUM, MPI_COMM_WORLD );
+        rc = MPI_Reduce_scatter_block( snd_buffer, rcv_buffer, count, type, op, MPI_COMM_WORLD );
         t1 = MPI_Wtime();
         assert ( rc == MPI_SUCCESS );
 
         for ( i = 0 ; i < count ; i++ ) assert( rcv_buffer[i] == rank );
 
         if ( rank == 0 ) printf( "%35s: %10u bytes (%7u bytes per rank) transferred in %6.2e seconds (%6.2e MB/s or %6.2e MB/s bytes per rank)\n",
-                                 "MPI_Reduce_scatter_block(MPI_SUM)",
+                                 "MPI_Reduce_scatter_block(op)",
                                  size * count * (int) sizeof(int), count * (int) sizeof(int), 
                                  t1 - t0, 
                                  1e-6 * size * count * sizeof(int) / (t1-t0),
@@ -490,7 +464,10 @@ int main(int argc, char *argv[])
 #else
 #    warning MPI_Reduce_scatter_block requires MPI-2.2
 #endif
+}
 
+void test_reduce_scatter_jeff(int rank, int size, MPI_Datatype type, MPI_Op op)
+{
     /* allreduce bandwidth test */
     if ( rank == 0 ) printf( "begin reduce_scatter via allreduce+memcpy bandwidth test\n" );
     for ( count = min_count; count < max_count ; count *= 2 )
@@ -513,7 +490,7 @@ int main(int argc, char *argv[])
         MPI_Barrier( MPI_COMM_WORLD );
 
         t0 = MPI_Wtime();
-        rc = MPI_Allreduce( MPI_IN_PLACE, snd_buffer, size * count, MPI_INT, MPI_SUM, MPI_COMM_WORLD );
+        rc = MPI_Allreduce( MPI_IN_PLACE, snd_buffer, size * count, type, op, MPI_COMM_WORLD );
         //for ( i = 0 ; i < count ; i++) rcv_buffer[i] = snd_buffer[ rank * count + i ];
         memcpy(&rcv_buffer[0],&snd_buffer[rank*count],count*sizeof(int));
         t1 = MPI_Wtime();
@@ -522,7 +499,7 @@ int main(int argc, char *argv[])
         for ( i = 0 ; i < count ; i++ ) assert( rcv_buffer[i] == rank );
 
         if ( rank == 0 ) printf( "%35s: %10u bytes (%7u bytes per rank) transferred in %6.2e seconds (%6.2e MB/s or %6.2e MB/s bytes per rank)\n",
-                                 "MPI_Allreduce(MPI_SUM)+memcpy",
+                                 "MPI_Allreduce(op)+memcpy",
                                  size * count * (int) sizeof(int), count * (int) sizeof(int), 
                                  t1 - t0, 
                                  1e-6 * size * count * sizeof(int) / (t1-t0),
@@ -531,7 +508,86 @@ int main(int argc, char *argv[])
         free(snd_buffer);
         free(rcv_buffer);
     }
+}
 
+int main(int argc, char *argv[])
+{
+    int provided;
+
+    MPI_Init_thread( &argc, &argv, MPI_THREAD_SINGLE, &provided );
+    assert( provided == MPI_THREAD_SINGLE );
+
+    MPI_Comm_size( MPI_COMM_WORLD, &size );
+    MPI_Comm_rank( MPI_COMM_WORLD, &rank );
+
+    //printf( "Hello from %d of %d processors\n", rank, size );
+    //fflush( stdout );
+
+    min_count = (argc > 1 ? atoi(argv[1]) : 1);
+    max_count = (argc > 2 ? atoi(argv[2]) : 1024);
+
+    if ( rank == 0 ) printf( "world size=%d min_count=%d max_count=%d\n", size , min_count , max_count );
+#ifdef __bgp__
+    {
+        _BGP_Personality_t pers;
+        char* mode = "WTF ";
+        char* torusX = "?";
+        char* torusY = "?";
+        char* torusZ = "?";
+        int sizeX = -1;
+        int sizeY = -1;
+        int sizeZ = -1;
+
+        if (rank == 0)
+        {
+             Kernel_GetPersonality(&pers, sizeof(pers));
+          
+                  if ( BGP_Personality_processConfig(&pers) == _BGP_PERS_PROCESSCONFIG_SMP ) mode = "SMP ";
+             else if ( BGP_Personality_processConfig(&pers) == _BGP_PERS_PROCESSCONFIG_VNM ) mode = "VNM ";
+             else if ( BGP_Personality_processConfig(&pers) == _BGP_PERS_PROCESSCONFIG_2x2 ) mode = "DUAL";
+          
+             torusX = BGP_Personality_isTorusX(&pers) ? "T" : "F";
+             torusY = BGP_Personality_isTorusY(&pers) ? "T" : "F";
+             torusZ = BGP_Personality_isTorusZ(&pers) ? "T" : "F";
+          
+             sizeX = BGP_Personality_xSize(&pers);
+             sizeY = BGP_Personality_ySize(&pers);
+             sizeZ = BGP_Personality_zSize(&pers);
+          
+             printf("mode %s topology (%2d,%2d,%2d) torus? (%1s,%1s,%1s)\n",
+                     mode, sizeX, sizeY, sizeZ, torusX, torusY, torusZ );
+        }
+    }
+#endif
+    fflush( stdout );
+
+    {
+        int t, o;
+        MPI_Datatype type;
+        MPI_Datatype types[4] = {MPI_INT, MPI_LONG, MPI_FLOAT, MPI_DOUBLE};
+        MPI_Op op;
+        MPI_Op ops[2] = {MPI_SUM, MPI_PROD};
+
+        for (t=0;t<4;t++)
+            for (o=0;o<2;o++)
+            {
+                type = types[t];
+                op = ops[o];
+
+                test_sendrecv(rank, size, type, op);
+                test_reduce(rank, size, type, op);
+                test_allreduce(rank, size, type, op);
+                test_scatter(rank, size, type, op);
+                test_scatterv(rank, size, type, op);
+                test_reduce_then_scatter(rank, size, type, op);
+                test_reduce_then_scatterv(rank, size, type, op);
+                test_reduce_scatter(rank, size, type, op);
+                test_reduce_scatter_block(rank, size, type, op);
+                test_reduce_scatter_jeff(rank, size, type, op);
+            }
+    }
+
+    MPI_Barrier( MPI_COMM_WORLD );
     if ( rank == 0 ) printf( "done with all tests\n" );
     fflush( stdout );
 
