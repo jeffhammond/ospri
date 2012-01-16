7fc83b0c8ae56c23f271f354125b036a3f4b5cc1
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2012-01-16 18:56:09 +0000

    slowly getting to some mild understanding of shm_open etc.
    git-svn-id: https://svn.mcs.anl.gov/repos/ospri@800 4a0c8edc-5189-0410-a5da-cd986011a6a8

---
+++ trunk/tests/devices/posix_shm/mmap_shm.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <assert.h>
+
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <pthread.h>
+
+//#define DEV_SHM
+#define POSIX_SHM
+
+int main(int argc, char* argv[])
+{
+    size_t size = ( argc>1 ? atoi(argv[1]) : getpagesize() ); 
+    printf("size = %ld \n", (long)size);
+
+#if defined(POSIX_SHM)
+    int fd = shm_open("/foo", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR );
+    if (fd<0) fprintf(stderr,"shm_open failed: %d \n", fd);
+    else      fprintf(stderr,"shm_open succeeded: %d \n", fd);
+#elif defined(DEV_SHM)
+    int fd = open("/dev/shm/foo", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR );
+    if (fd<0) fprintf(stderr,"open failed: %d \n", fd);
+    else      fprintf(stderr,"open succeeded: %d \n", fd);
+
+    if (fd>=0)
+    {
+        int rc = ftruncate(fd, size);
+        if (rc==0) fprintf(stderr,"ftruncate succeeded \n");
+        else       fprintf(stderr,"ftruncate failed \n");
+    }
+#else
+    int fd = -1;
+    fprintf(stderr,"no file backing \n");
+#endif
+
+#if defined(DEV_SHM)
+    void * ptr = mmap( NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0 );
+#else
+    void * ptr = mmap( NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, fd, 0 );
+#endif
+    if (ptr==NULL) fprintf(stderr,"mmap failed \n");
+    else           fprintf(stderr,"mmap succeeded \n");
+    
+    memset(ptr, '\0', size);
+    fprintf(stderr,"memset succeeded \n");
+
+#if defined(POSIX_SHM)
+    if (fd>=0)
+    {
+        int rc = shm_unlink("/foo");
+        if (rc==0) fprintf(stderr,"shm_unlink succeeded \n");
+        else       fprintf(stderr,"shm_unlink failed \n");
+    }
+#elif defined(DEV_SHM)
+    if (fd>=0)
+    {
+        int rc = -1;
+
+        rc = ftruncate(fd, 0);
+        if (rc==0) fprintf(stderr,"ftruncate succeeded \n");
+        else       fprintf(stderr,"ftruncate failed \n");
+
+        rc = close(fd);
+        if (rc==0) fprintf(stderr,"close succeeded \n");
+        else       fprintf(stderr,"close failed \n");
+    }
+#endif
+
+    if (ptr!=NULL)
+    {
+        int rc = munmap(ptr, size);
+        if (rc==0) fprintf(stderr,"munmap succeeded \n");
+        else       fprintf(stderr,"munmap failed \n");
+    }
+
+    return 0;
+}
--- trunk/tests/devices/posix_shm/test_basic.c
+++ trunk/tests/devices/posix_shm/test_basic.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 #include <fcntl.h>
 #include <unistd.h>
 
@@ -15,9 +16,13 @@ int main(int argc, char * argv[])
     long size = ( argc>1 ? atol(argv[1]) : (long)getpagesize() ); 
     printf("size = %ld \n", size);
 
-    int fd = shm_open("/foo", O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR );
-    if (fd<0) fprintf(stderr,"shm_open failed: %d \n", fd);
-    else      fprintf(stderr,"shm_open succeeded: %d \n", fd);
+    //int fd = shm_open("./foo", O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR );
+    //if (fd<0) fprintf(stderr,"shm_open failed: %d \n", fd);
+    //else      fprintf(stderr,"shm_open succeeded: %d \n", fd);
+
+    int fd = open("./foo", O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR );
+    if (fd<0) fprintf(stderr,"open failed: %d \n", fd);
+    else      fprintf(stderr,"open succeeded: %d \n", fd);
 
     int rc = close(fd);
     if (rc==0) fprintf(stderr,"close succeeded \n");
