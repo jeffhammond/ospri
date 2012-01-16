d3a2180b21aed3e81af46729d57fa08a061e01f5
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2012-01-16 16:10:04 +0000

    figuring out posix shm
    git-svn-id: https://svn.mcs.anl.gov/repos/ospri@799 4a0c8edc-5189-0410-a5da-cd986011a6a8

---
+++ trunk/tests/devices/posix_shm/test_basic.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/*
+int shm_open(const char *name, int oflag, mode_t mode);
+int shm_unlink(const char *name);
+*/
+
+int main(int argc, char * argv[])
+{
+    long size = ( argc>1 ? atol(argv[1]) : (long)getpagesize() ); 
+    printf("size = %ld \n", size);
+
+    int fd = shm_open("/foo", O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR );
+    if (fd<0) fprintf(stderr,"shm_open failed: %d \n", fd);
+    else      fprintf(stderr,"shm_open succeeded: %d \n", fd);
+
+    int rc = close(fd);
+    if (rc==0) fprintf(stderr,"close succeeded \n");
+    else       fprintf(stderr,"close failed \n");
+
+    return 0;
+}
+
