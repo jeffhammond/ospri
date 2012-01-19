86ec0a0132550b3e00c0b4ca4ad81d18f84421fe
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2012-01-19 22:16:36 +0000

    mod is pointless cleanup timer source
    git-svn-id: https://svn.mcs.anl.gov/repos/ospri@824 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- trunk/src/util/Makefile.mk
+++ trunk/src/util/Makefile.mk
@@ -5,4 +5,5 @@
 
 libosp_la_SOURCES += $(top_srcdir)/src/util/ospu_local_put.c \
                      $(top_srcdir)/src/util/ospu_local_get.c \
+                     $(top_srcdir)/src/util/ospu_local_acc.c \
                      $(top_srcdir)/src/util/ospu_timer.c
--- trunk/src/util/ospu_timer.c
+++ trunk/src/util/ospu_timer.c
@@ -8,55 +8,47 @@
  */
 
 #if defined(__GNUC__)
+#error "Requires GNU-style ASM!"
+#endif
 
-    #if defined(__i386__)
+unsigned long long OSPU_Time_cycles(void)
+{
+#if defined(__i386__)
 
-        unsigned long long OSPU_Time_cycles(void)
-        {
-          unsigned long long int x;
-             __asm__ volatile (".byte 0x0f, 0x31" : "=A" (x));
-             return x;
-        }
+    unsigned long long int x;
+    __asm__ volatile (".byte 0x0f, 0x31" : "=A" (x));
+    return x;
 
-        #elif defined(__x86_64__)
+#elif defined(__x86_64__)
 
-        unsigned long long OSPU_Time_cycles(void)
-        {
-          unsigned hi, lo;
-          __asm__ __volatile__ ("rdtsc" : "=a"(lo), "=d"(hi));
-          return ( (unsigned long long)lo)|( ((unsigned long long)hi)<<32 );
-        }
+    unsigned hi, lo;
+    __asm__ __volatile__ ("rdtsc" : "=a"(lo), "=d"(hi));
+    return ( (unsigned long long)lo)|( ((unsigned long long)hi)<<32 );
 
-        #elif defined(__powerpc__)
+#elif defined(__powerpc__)
 
-        unsigned long long OSPU_Time_cycles(void)
-        {
-          unsigned long long int result=0;
-          unsigned long int upper, lower,tmp;
-          __asm__ volatile(
-                        "0:                  \n"
-                        "\tmftbu   %0           \n"
-                        "\tmftb    %1           \n"
-                        "\tmftbu   %2           \n"
-                        "\tcmpw    %2,%0        \n"
-                        "\tbne     0b         \n"
-                        : "=r"(upper),"=r"(lower),"=r"(tmp)
-                        );
-          result = upper;
-          result = result<<32;
-          result = result|lower;
-
-          return(result);
-        }
-
-    #else
-
-        #error "No tick counter is available!"
-
-    #endif
+    unsigned long long int result=0;
+    unsigned long int upper, lower,tmp;
+    __asm__ volatile(
+            "0:                  \n"
+            "\tmftbu   %0           \n"
+            "\tmftb    %1           \n"
+            "\tmftbu   %2           \n"
+            "\tcmpw    %2,%0        \n"
+            "\tbne     0b         \n"
+            : "=r"(upper),"=r"(lower),"=r"(tmp)
+    );
+    result = upper;
+    result = result<<32;
+    result = result|lower;
+    return(result);
 
 #else
 
-    #error "Requires GNU-style ASM!"
+#warning "No tick counter is available!"
+    return 0;
 
 #endif
+}
+
+
