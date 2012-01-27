5137a46118a2c3a319013cfa1af18f307b3a81ba
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2012-01-27 00:19:52 +0000

    cleanup
    git-svn-id: https://svn.mcs.anl.gov/repos/ospri@881 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- branches/transpose/hpm.h
+++ branches/transpose/hpm.h
@@ -6,12 +6,4 @@ void HPM_Print(void);                                  /*   print counter values
 void HPM_Print_Flops(void);                            /*   print flops                      */
 void HPM_Print_Flops_Agg(void);                        /*   print aggregate flops file       */
 void HPM_Flops(char *label, float f0, float f1 );      /*   return flops                     */
-#else
-#define HPM_Init
-#define HPM_Start
-#define HPM_Stop
-#define HPM_Print
-#define HPM_Print_Flops
-#define HPM_Print_Flops_Agg
-#define HPM_Flops
 #endif
