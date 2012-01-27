c53d8f23acab4d6b26edae9ab3dcb135f768ef6d
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2012-01-27 00:26:42 +0000

    add extern
    git-svn-id: https://svn.mcs.anl.gov/repos/ospri@882 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- branches/transpose/hpm.h
+++ branches/transpose/hpm.h
@@ -1,3 +1,7 @@
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
 #ifdef __bgp__
 void HPM_Init(void);                                   /*   initialize the counters          */
 void HPM_Start(char *label);                           /*   start counting                   */
@@ -6,4 +10,10 @@ void HPM_Print(void);                                  /*   print counter values
 void HPM_Print_Flops(void);                            /*   print flops                      */
 void HPM_Print_Flops_Agg(void);                        /*   print aggregate flops file       */
 void HPM_Flops(char *label, float f0, float f1 );      /*   return flops                     */
+#else
+#error This header file must be use on Blue Gene/P.
+#endif
+
+#if defined(__cplusplus)
+}
 #endif
