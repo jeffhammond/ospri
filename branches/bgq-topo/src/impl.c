9d2a5eb964032e08b326c06ce4b4381ed76f79f4
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2012-04-03 15:58:14 +0000

    init done
    git-svn-id: https://svn.mcs.anl.gov/repos/ospri@922 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- branches/bgq-topo/src/impl.c
+++ branches/bgq-topo/src/impl.c
@@ -1,4 +1,6 @@
-#include "torus.h"
+#include "impl.h"
+
+BGQ_Torus_t info;
 
 void BGQTopo_Init(void)
 {
@@ -13,97 +15,40 @@ void BGQTopo_Init(void)
     rc = Kernel_JobCoords(&jobcoords);
     assert(rc==0);
 
-    printf("%d: pers.Network_Config.Acoord = %d \n", rank, pers.Network_Config.Acoord);
-    printf("%d: pers.Network_Config.Bcoord = %d \n", rank, pers.Network_Config.Bcoord);
-    printf("%d: pers.Network_Config.Ccoord = %d \n", rank, pers.Network_Config.Ccoord);
-    printf("%d: pers.Network_Config.Dcoord = %d \n", rank, pers.Network_Config.Dcoord);
-    printf("%d: pers.Network_Config.Ecoord = %d \n", rank, pers.Network_Config.Ecoord);
-    fflush(stdout);
+    info.Coords[0] = pers.Network_Config.Acoord;
+    info.Coords[1] = pers.Network_Config.Bcoord;
+    info.Coords[2] = pers.Network_Config.Ccoord;
+    info.Coords[3] = pers.Network_Config.Dcoord;
+    info.Coords[4] = pers.Network_Config.Ecoord;
+    info.Coords[5] = Kernel_ProcessorID();
 
-    if (rank==0)
-    {
-        printf("%d: pers.Network_Config.Anodes = %d \n", rank, pers.Network_Config.Anodes);
-        printf("%d: pers.Network_Config.Bnodes = %d \n", rank, pers.Network_Config.Bnodes);
-        printf("%d: pers.Network_Config.Cnodes = %d \n", rank, pers.Network_Config.Cnodes);
-        printf("%d: pers.Network_Config.Dnodes = %d \n", rank, pers.Network_Config.Dnodes);
-        printf("%d: pers.Network_Config.Enodes = %d \n", rank, pers.Network_Config.Enodes);
+    info.PartitionSize[0] = pers.Network_Config.Anodes;
+    info.PartitionSize[1] = pers.Network_Config.Bnodes;
+    info.PartitionSize[2] = pers.Network_Config.Cnodes;
+    info.PartitionSize[3] = pers.Network_Config.Dnodes;
+    info.PartitionSize[4] = pers.Network_Config.Enodes;
+    info.PartitionSize[5] = Kernel_ProcessCount(); 
 
-        printf("%d: ND_GET_TORUS(0,pers.Network_Config.NetFlags) = %d \n", rank, ND_GET_TORUS(0,pers.Network_Config.NetFlags) );
-        printf("%d: ND_GET_TORUS(1,pers.Network_Config.NetFlags) = %d \n", rank, ND_GET_TORUS(1,pers.Network_Config.NetFlags) );
-        printf("%d: ND_GET_TORUS(2,pers.Network_Config.NetFlags) = %d \n", rank, ND_GET_TORUS(2,pers.Network_Config.NetFlags) );
-        printf("%d: ND_GET_TORUS(3,pers.Network_Config.NetFlags) = %d \n", rank, ND_GET_TORUS(3,pers.Network_Config.NetFlags) );
-        printf("%d: ND_GET_TORUS(4,pers.Network_Config.NetFlags) = %d \n", rank, ND_GET_TORUS(4,pers.Network_Config.NetFlags) );
+    info.PartitionTorus[0] = ND_GET_TORUS(0,pers.Network_Config.NetFlags);
+    info.PartitionTorus[1] = ND_GET_TORUS(1,pers.Network_Config.NetFlags);
+    info.PartitionTorus[2] = ND_GET_TORUS(2,pers.Network_Config.NetFlags);
+    info.PartitionTorus[3] = ND_GET_TORUS(3,pers.Network_Config.NetFlags);
+    info.PartitionTorus[4] = ND_GET_TORUS(4,pers.Network_Config.NetFlags);
+    info.PartitionTorus[5] = 1;
 
-        printf("%d: jobcoords.isSubBlock = %d \n", rank, jobcoords.isSubBlock);
+    info.JobSize[0] = jobcoords.shape.a;
+    info.JobSize[1] = jobcoords.shape.b;
+    info.JobSize[2] = jobcoords.shape.c;
+    info.JobSize[3] = jobcoords.shape.d;
+    info.JobSize[4] = jobcoords.shape.e;
+    info.JobSize[5] = jobcoords.shape.core;
 
-        if (jobcoords.isSubBlock==1)
-        {
-            printf("%d: jobcoords.corner.a = %d \n", rank, jobcoords.corner.a);
-            printf("%d: jobcoords.corner.b = %d \n", rank, jobcoords.corner.b);
-            printf("%d: jobcoords.corner.c = %d \n", rank, jobcoords.corner.c);
-            printf("%d: jobcoords.corner.d = %d \n", rank, jobcoords.corner.d);
-            printf("%d: jobcoords.corner.e = %d \n", rank, jobcoords.corner.e);
-            printf("%d: jobcoords.corner.t = %d \n", rank, jobcoords.corner.core);
- 
-            printf("%d: jobcoords.shape.a = %d \n", rank, jobcoords.shape.a);
-            printf("%d: jobcoords.shape.b = %d \n", rank, jobcoords.shape.b);
-            printf("%d: jobcoords.shape.c = %d \n", rank, jobcoords.shape.c);
-            printf("%d: jobcoords.shape.d = %d \n", rank, jobcoords.shape.d);
-            printf("%d: jobcoords.shape.e = %d \n", rank, jobcoords.shape.e);
-            printf("%d: jobcoords.shape.t = %d \n", rank, jobcoords.shape.core);
+    info.JobTorus[0] = ND_GET_TORUS(0,pers.Network_Config.NetFlags) && jobcoords.shape.a==pers.Network_Config.Anodes;
+    info.JobTorus[1] = ND_GET_TORUS(1,pers.Network_Config.NetFlags) && jobcoords.shape.b==pers.Network_Config.Bnodes;
+    info.JobTorus[2] = ND_GET_TORUS(2,pers.Network_Config.NetFlags) && jobcoords.shape.c==pers.Network_Config.Cnodes;
+    info.JobTorus[3] = ND_GET_TORUS(3,pers.Network_Config.NetFlags) && jobcoords.shape.d==pers.Network_Config.Dnodes;
+    info.JobTorus[4] = ND_GET_TORUS(4,pers.Network_Config.NetFlags) && jobcoords.shape.e==pers.Network_Config.Enodes;
+    info.JobTorus[5] = 1;
 
-            printf("%d: subjob is torus along a = %d \n", rank, ND_GET_TORUS(0,pers.Network_Config.NetFlags) && jobcoords.shape.a==pers.Network_Config.Anodes);
-            printf("%d: subjob is torus along b = %d \n", rank, ND_GET_TORUS(1,pers.Network_Config.NetFlags) && jobcoords.shape.b==pers.Network_Config.Bnodes);
-            printf("%d: subjob is torus along c = %d \n", rank, ND_GET_TORUS(2,pers.Network_Config.NetFlags) && jobcoords.shape.c==pers.Network_Config.Cnodes);
-            printf("%d: subjob is torus along d = %d \n", rank, ND_GET_TORUS(3,pers.Network_Config.NetFlags) && jobcoords.shape.d==pers.Network_Config.Dnodes);
-            printf("%d: subjob is torus along e = %d \n", rank, ND_GET_TORUS(4,pers.Network_Config.NetFlags) && jobcoords.shape.e==pers.Network_Config.Enodes);
-        }
-        fflush(stdout);
-    }
-
-    MPI_Finalize();
-
-    return 0;
+    return;
 }
-
-/*=======================================*/
-/* routine to return the BGQ core number */
-/*=======================================*/
-int get_bgq_core(void)
-{
-#ifdef BGQ
-    //int core = Kernel_PhysicalProcessorID();
-    int core = Kernel_ProcessorCoreID();
-    return core;
-#else
-    return -1;
-#endif
-}
-
-/*==========================================*/
-/* routine to return the BGQ hwthread (0-3) */
-/*==========================================*/
-int get_bgq_hwthread(void)
-{
-#ifdef BGQ
-    //int hwthread = Kernel_PhysicalHWThreadID();
-    int hwthread = Kernel_ProcessorThreadID();
-    return hwthread;
-#else
-    return -1;
-#endif
-}
-
-/*======================================================*/
-/* routine to return the BGQ virtual core number (0-67) */
-/*======================================================*/
-int get_bgq_vcore(void)
-{
-#ifdef BGQ
-    int hwthread = Kernel_ProcessorID();
-    return hwthread;
-#else
-    return -1;
-#endif
-}
-
--- branches/bgq-topo/src/impl.h
+++ branches/bgq-topo/src/impl.h
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <unistd.h>
+#include <assert.h>
 
 #include <process.h>
 #include <location.h>
 #include <personality.h>
 
+struct BGQ_Torus_t
+{
+    int32_t Coords[6];    
+    int32_t PartitionSize[6];    
+    int32_t PartitionTorus[6];    
+    int32_t JobSize[6];    
+    int32_t JobTorus[6];    
+} 
+BGQ_Torus_t
