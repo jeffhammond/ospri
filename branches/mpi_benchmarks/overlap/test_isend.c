58833cb4208a97833b413f21d2ac6cb9854f75e3
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2012-04-03 17:50:43 +0000

    make a better test
    git-svn-id: https://svn.mcs.anl.gov/repos/ospri@924 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- branches/mpi_benchmarks/overlap/test_isend.c
+++ branches/mpi_benchmarks/overlap/test_isend.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <assert.h>
 
 #include <mpi.h>
@@ -8,57 +9,73 @@ int main(int argc, char * argv[])
 {
     int rc, rc1, rc2;
 
-    int requested = MPI_THREAD_SINGLE;
-    int provided = -1;
-    rc = MPI_Init_thread(&argc, &argv, requested, &provided);
-    assert(rc==MPI_SUCCESS);
+    int requested = MPI_THREAD_SINGLE, provided;
+    MPI_Init_thread(&argc, &argv, requested, &provided);
     assert(provided>=requested);
 
-    int size = -1;
-    rc = MPI_Comm_size(MPI_COMM_WORLD, &size);
-    assert(rc==MPI_SUCCESS);
-    assert(size>1);
+    int r, rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    int rank = -1;
-    rc = MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    assert(rc==MPI_SUCCESS);
+    int c, d, count = ( argc > 1 ? atoi(argv[1]) : 1024*1024 );
 
-    int maxbytes = ( argc > 1 ? atoi(argv[1]) : 1024*1024 );
+    double * rbuf = malloc( count * sizeof(double) );
+    double * sbuf = malloc( count * sizeof(double) );
+    assert(rbuf!=NULL && sbuf!=NULL);
 
-    int n = 0;
-    for ( b=1; b<maxbytes; b*=2 ) n++;
+    for (d=0; d<count; d++) sbuf[d] = (double)size; 
 
-    double * baseline = malloc(n*sizeof(double));
-
-
-    for ( b=1; b<maxbytes; b*=2 )
+    for (c=1; c<count; c*=2)
     {
+        double t0, t1;
+
+        for (r=0; r<size; r++)
+        {
+            int tag = 1000;
+            MPI_Request req[2];
+
+            memset( rbuf, '\0', count*sizeof(double) );
+
+            t0 = MPI_Wtime();
+
+            if (rank==r)
+                MPI_Irecv(rbuf, c, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD, &req[0]);
+
+            if (rank==0)
+                MPI_Isend(sbuf, c, MPI_DOUBLE, r, tag, MPI_COMM_WORLD, &req[1]);
+
+            if (rank==r)
+		MPI_Wait( &req[0], MPI_STATUS_IGNORE );
+
+            if (rank==0)
+		MPI_Wait( &req[1], MPI_STATUS_IGNORE );
+
+            t1 = MPI_Wtime();
+
+            if (rank==r)
+            {
+                int errors = 0;
+                for (d=0; d<c; d++) 
+                    errors += (rbuf[d] != (double)size );
+                if (errors>0) printf("There were %d errors! \n", errors);
+            }
+
+            if (rank==0)
+                printf("%d->%d: bytes = %d bandwidth = %lf MB/s \n", 0, r, c*sizeof(double), 1e-6*c*sizeof(double)/(t1-t0) );
+
+        }
         if (rank==0)
         {
-            double t0=0.0, t1=0.0;
-            MPI_Request req;
             t0 = MPI_Wtime();
-            rc1 = MPI_Isend(buf, b, MPI_BYTE, 1, tag, MPI_COMM_WORLD, &req);
-            rc2 = MPI_Wait(&req, MPI_STATUS_IGNORE);
+            memcpy( rbuf, sbuf, c*sizeof(double) );
             t1 = MPI_Wtime();
-            assert(rc1==MPI_SUCCESS && rc2=MPI_SUCCESS);
-
+            printf("memcpy: bytes = %d bandwidth = %lf MB/s \n", c*sizeof(double), 1e-6*c*sizeof(double)/(t1-t0) );
         }
-        else if (rank==1)
-        {
-            rc = MPI_Recv(buf, b, MPI_BYTE, 0, tag, MPI_COMM_WORLD, MPI_STATUS_NULL);
-            assert(rc==MPI_SUCCESS);
-        }
-        else
-        {
-        }
-
-        rc = MPI_Barrier(MPI_COMM_WORLD);
-        assert(rc==MPI_SUCCESS);
+        fflush(stdout);
+        MPI_Barrier(MPI_COMM_WORLD);
     }
 
-    rc = MPI_Finalize();
-    assert(rc==MPI_SUCCESS);
+    MPI_Finalize();
 
     return 0;
 }
