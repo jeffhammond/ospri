dbf4a715b83978bb0e07a289e471fc3663203d11
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2012-04-03 15:34:31 +0000

    oops
    git-svn-id: https://svn.mcs.anl.gov/repos/ospri@921 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- branches/bgq-topo/src/impl.c
+++ branches/bgq-topo/src/impl.c
@@ -65,3 +65,45 @@ void BGQTopo_Init(void)
 
     return 0;
 }
+
+/*=======================================*/
+/* routine to return the BGQ core number */
+/*=======================================*/
+int get_bgq_core(void)
+{
+#ifdef BGQ
+    //int core = Kernel_PhysicalProcessorID();
+    int core = Kernel_ProcessorCoreID();
+    return core;
+#else
+    return -1;
+#endif
+}
+
+/*==========================================*/
+/* routine to return the BGQ hwthread (0-3) */
+/*==========================================*/
+int get_bgq_hwthread(void)
+{
+#ifdef BGQ
+    //int hwthread = Kernel_PhysicalHWThreadID();
+    int hwthread = Kernel_ProcessorThreadID();
+    return hwthread;
+#else
+    return -1;
+#endif
+}
+
+/*======================================================*/
+/* routine to return the BGQ virtual core number (0-67) */
+/*======================================================*/
+int get_bgq_vcore(void)
+{
+#ifdef BGQ
+    int hwthread = Kernel_ProcessorID();
+    return hwthread;
+#else
+    return -1;
+#endif
+}
+
