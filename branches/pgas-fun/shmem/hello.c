a66d79320b2068d9eb266b62306bc33de9e59b07
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2012-07-15 15:58:54 +0000

    shmem_finalize is not part of OpenSHMEM
    git-svn-id: https://svn.mcs.anl.gov/repos/ospri@1011 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- branches/pgas-fun/shmem/hello.c
+++ branches/pgas-fun/shmem/hello.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-#ifdef OPENSHMEM
-#include <shmem.h>
-#else
 #include <mpp/shmem.h>
-#endif
 
 int main(int argc, char* argv[])
 {
     start_pes(0);
+
 #ifdef OPENSHMEM
     printf("Hello world: I am PE %d of %d.\n", _my_pe(), _num_pes());
 #else
     printf("Hello world: I am PE %d of %d.\n", my_pe(), num_pes());
 #endif
+
     shmem_barrier_all();
-    shmem_finalize();
+
     return 0;
 }
