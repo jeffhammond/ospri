901e2e287601b57f25741456440d25bda701021f
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2012-07-16 19:05:59 +0000

    fix discrepancy between Cray and Open SHMEM
    git-svn-id: https://svn.mcs.anl.gov/repos/ospri@1012 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- branches/pgas-fun/shmem/hello.c
+++ branches/pgas-fun/shmem/hello.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <mpp/shmem.h>
+#include "myshmem.h"
 
 int main(int argc, char* argv[])
 {
-    start_pes(0);
-
-#ifdef OPENSHMEM
-    printf("Hello world: I am PE %d of %d.\n", _my_pe(), _num_pes());
-#else
+    shmem_init();
     printf("Hello world: I am PE %d of %d.\n", my_pe(), num_pes());
-#endif
-
     shmem_barrier_all();
-
+    shmem_finalize();
     return 0;
 }
