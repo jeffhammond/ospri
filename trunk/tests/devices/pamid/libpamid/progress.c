a6784500cd8e162222a13331b285f01c77c796e9
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2012-09-18 23:21:59 +0000

    git-svn-id: https://svn.mcs.anl.gov/repos/ospri@1088 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- trunk/tests/devices/pamid/libpamid/progress.c
+++ trunk/tests/devices/pamid/libpamid/progress.c
@@ -14,7 +14,7 @@ static pami_result_t (*async_prog_register)(pami_context_t context,
 static pami_result_t (*async_prog_enable)(pami_context_t context, pamix_async_t event_type);
 static pami_result_t (*async_prog_disable)(pami_context_t context, pamix_async_t event_type);
 
-pami_result_t PAMID_Progess_init()
+int PAMID_Progess_setup(void)
 {
 	pami_result_t rc = PAMI_ERROR;
 
@@ -22,20 +22,23 @@ pami_result_t PAMID_Progess_init()
 	PAMID_ASSERT(rc==PAMI_SUCCESS,"PAMI_Extension_open - EXT_async_progress");
 
 	async_prog_register = PAMI_Extension_symbol(async_prog_ext, "register");
-	if (async_prog_register == NULL) {
-		fprintf(stderr, "PAMI extension EXT_async_progress has no 'register' func\n");
-		return PAMI_ERROR;
-	}
+	PAMID_ASSERT(async_prog_register!=NULL,"PAMI_Extension_symbol - async_prog_register");
+
 	async_prog_enable = PAMI_Extension_symbol(async_prog_ext, "enable");
-	if (async_prog_enable == NULL) {
-		fprintf(stderr, "PAMI extension EXT_async_progress has no 'enable' func\n");
-		return PAMI_ERROR;
-	}
+	PAMID_ASSERT(async_prog_enable!=NULL,"PAMI_Extension_symbol - async_prog_enable");
+
 	async_prog_disable = PAMI_Extension_symbol(async_prog_ext, "disable");
-	if (async_prog_disable == NULL) {
-		fprintf(stderr, "PAMI extension EXT_async_progress has no 'disable' func\n");
-		return PAMI_ERROR;
-	}
+	PAMID_ASSERT(async_prog_disable!=NULL,"PAMI_Extension_symbol - async_prog_disable");
+
 	return PAMI_SUCCESS;
+}
+
+int PAMID_Progess_teardown()
+{
+  pami_result_t rc = PAMI_ERROR;
+  rc = PAMI_Extension_close(async_prog_ext);
+  PAMID_ASSERT(rc==PAMI_SUCCESS,"PAMI_Extension_close - EXT_async_progress");
+
+  return;
 }
 
