5068db7c296cc4d366be1b625489f76cbfa642e8
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2012-09-18 20:38:06 +0000

    fixes
    git-svn-id: https://svn.mcs.anl.gov/repos/ospri@1081 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- trunk/tests/devices/pamid/libpamid/pamiu.h
+++ trunk/tests/devices/pamid/libpamid/pamiu.h
@@ -3,12 +3,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int posix_memalign(void ** memptr, size_t alignment, size_t size);
 
 #define ALIGNMENT 128
 
-void * PAMIU_Malloc(size_t);
-void PAMIU_Free(void *)
+void * PAMIU_Malloc(size_t size);
+void PAMIU_Free(void * ptr);
 
 #endif /* PAMIU_H */
