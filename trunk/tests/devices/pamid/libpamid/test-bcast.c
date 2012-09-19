6980f7afbff71ae8b69a7e189e8f1133240ab7f4
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2012-09-19 20:11:43 +0000

    bcast test
    git-svn-id: https://svn.mcs.anl.gov/repos/ospri@1128 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- trunk/tests/devices/pamid/libpamid/test-bcast.c
+++ trunk/tests/devices/pamid/libpamid/test-bcast.c
@@ -4,7 +4,28 @@ int main(int argc, char * argv[])
 {
 	PAMID_Initialize();
 
-	PAMID_Barrier_world();
+	const int n = 1000;
+	int * a = malloc(n*sizeof(int));
+
+	int r = (int)PAMID_World_rank();
+
+	if (r==0)
+		for (int i=0; i<n; i++)
+			a[i] = 37373737;
+	else
+		for (int i=0; i<n; i++)
+			a[i] = -1;
+
+	PAMID_Broadcast_world(0, n*sizeof(int), (void*) a);
+
+	int errors = 0;
+	for (int i=0; i<n; i++)
+		if (a[i] != 37373737)
+			errors++;
+
+	printf("rank %ld has %d errors \n", PAMID_World_rank(), errors );
+
+	free(a);
 
 	PAMID_Finalize();
 
