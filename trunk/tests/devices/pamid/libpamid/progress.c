001a1ab1379ec51d0510c0138a3ea58a4ac29e6b
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2012-09-19 01:19:32 +0000

    git-svn-id: https://svn.mcs.anl.gov/repos/ospri@1092 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- trunk/tests/devices/pamid/libpamid/progress.c
+++ trunk/tests/devices/pamid/libpamid/progress.c
@@ -1,44 +1,143 @@
 #include "pamid.h"
 
-static pami_extension_t async_prog_ext;
-
-typedef void (*pamix_async_function) (pami_context_t context, void * cookie);
-
-typedef enum { PAMI_ASYNC_ALL = 0 } pamix_async_t;
-
-static pami_result_t (*async_prog_register)(pami_context_t context,
-		pamix_async_function progress_fn,
-		pamix_async_function suspend_fn,
-		pamix_async_function resume_fn,
-		void *cookie);
-static pami_result_t (*async_prog_enable)(pami_context_t context, pamix_async_t event_type);
-static pami_result_t (*async_prog_disable)(pami_context_t context, pamix_async_t event_type);
-
-int PAMID_Progess_setup(void)
+typedef enum
 {
-	pami_result_t rc = PAMI_ERROR;
+	ASYNC_PROGRESS_EVENT_ALL            =    0,
+	ASYNC_PROGRESS_EVENT_RECV_INTERRUPT =    1,
+	ASYNC_PROGRESS_EVENT_TIMER          =    2,
+	ASYNC_PROGRESS_EVENT_EXT            = 1000
+} async_progress_event_t;
 
-	rc = PAMI_Extension_open(NULL, "EXT_async_progress", &async_prog_ext);
+typedef void (*async_progress_function) (pami_context_t context, void * cookie);
+
+typedef pami_rc_t (*async_progress_register_function) (pami_context_t context,
+		async_progress_function progress_fn,
+		async_progress_function suspend_fn,
+		async_progress_function resume_fn,
+		void * cookie);
+
+typedef pami_rc_t (*async_progress_enable_function) (pami_context_t context, async_progress_event_t event);
+
+typedef pami_rc_t (*async_progress_disable_function) (pami_context_t context, async_progress_event_t event);
+
+typedef struct
+{
+	pami_extension_t extension;
+	async_progress_register_function register_fn;
+	async_progress_enable_function   enable_fn;
+	async_progress_disable_function  disable_fn;
+} async_progress_impl_t;
+
+void async_progress_open(async_progress_t * async_progress)
+{
+	pami_rc_t rc = PAMI_ERROR;
+	async_progress_impl_t * async = (async_progress_impl_t *) async_progress;
+
+	/* Sanity check that the opaque object is large enough to contain the internal async progress 'handle' structure. */
+	PAMID_ASSERT(sizeof(async_progress_impl_t) <= sizeof(async_progress_t),"async_progress_open");
+
+	/* Open the async progress extension. */
+	rc = PAMI_Extension_open(PAMID_INTERNAL_STATE.client, "EXT_async_progress", (pami_extension_t *) &async->extension);
 	PAMID_ASSERT(rc==PAMI_SUCCESS,"PAMI_Extension_open - EXT_async_progress");
 
-	async_prog_register = PAMI_Extension_symbol(async_prog_ext, "register");
+	/* Get the various async progress extension functions.  */
+	async->register_fn = NULL;
+	async->register_fn = (async_progress_register_function) PAMI_Extension_symbol(async_progress_extension, "register");
 	PAMID_ASSERT(async_prog_register!=NULL,"PAMI_Extension_symbol - async_prog_register");
 
-	async_prog_enable = PAMI_Extension_symbol(async_prog_ext, "enable");
+	async->enable_fn = NULL;
+	async->enable_fn = (async_progress_enable_function) PAMI_Extension_symbol(async_progress_extension, "enable");
 	PAMID_ASSERT(async_prog_enable!=NULL,"PAMI_Extension_symbol - async_prog_enable");
 
-	async_prog_disable = PAMI_Extension_symbol(async_prog_ext, "disable");
+	async->disable_fn = NULL;
+	async->disable_fn = (async_progress_disable_function) PAMI_Extension_symbol(async_progress_extension, "disable");
 	PAMID_ASSERT(async_prog_disable!=NULL,"PAMI_Extension_symbol - async_prog_disable");
+
+	async->extension = async_progress_extension;
+
+	return;
+}
+
+void async_progress_close(async_progress_t * async_progress)
+{
+	pami_rc_t rc = PAMI_ERROR;
+	async_progress_impl_t * async = (async_progress_impl_t *) async_progress;
+
+	/* Close the async progress extension. */
+	rc = PAMI_Extension_close (async->extension);
+	PAMID_ASSERT(rc==PAMI_SUCCESS,"PAMI_Extension_close - EXT_async_progress");
+
+	return;
+}
+
+void async_progress_enable(async_progress_t * async_progress, pami_context_t context)
+{
+	pami_rc_t rc = PAMI_ERROR;
+	async_progress_impl_t * async = (async_progress_impl_t *) async_progress;
+
+	/* Register the async progress event handlers for this context. */
+	rc = PAMI_ERROR;
+	rc = async->register_fn(context,
+			NULL,    /* progress function */
+			NULL,    /* suspend function */
+			NULL,    /* resume function */
+			NULL);   /* cookie */
+	PAMID_ASSERT(rc==PAMI_SUCCESS,"async->register_fn");
+
+	/* Enable async progress for this context. */
+	rc = PAMI_ERROR;
+	rc = async->enable_fn(context, ASYNC_PROGRESS_EVENT_ALL);
+	PAMID_ASSERT(rc==PAMI_SUCCESS,"async->enable_fn");
+
+	/* Async progress is enabled when the async progress extension acquires the context lock. */
+	fprintf (stdout, "(%03d) Waiting for the async progress extension to acquire the context lock.\n", __LINE__);
+	do
+	{
+		rc = PAMI_ERROR;
+		rc = PAMI_Context_trylock (context);
+		PAMID_ASSERT(rc!=PAMI_ERROR,"PAMI_Context_trylock");
+
+		if (rc == PAMI_SUCCESS)
+			PAMI_Context_unlock (context);
+	}
+	while (rc == PAMI_SUCCESS);
+
+	return;
+}
+
+void async_progress_disable(async_progress_t * async_progress, pami_context_t context)
+{
+	pami_rc_t rc = PAMI_ERROR;
+	async_progress_impl_t * async = (async_progress_impl_t *) async_progress;
+
+	/* Sanity check that the context lock is held by the async progress extension. */
+	rc = PAMI_ERROR;
+	rc = PAMI_Context_trylock(context);
+	PAMID_ASSERT(rc==PAMI_EAGAIN,"PAMI_Context_trylock");
+
+	/* Disable async progress for this context. */
+	rc = PAMI_ERROR;
+	rc = async->disable_fn(context, ASYNC_PROGRESS_EVENT_ALL);
+	PAMID_ASSERT(rc==PAMI_SUCCESS,"async->disable_fn");
+
+	/* Block until the context is no longer under async progress. This occurs when the context lock is released? */
+	rc = PAMI_Context_lock(context);
+	PAMID_ASSERT(rc==PAMI_SUCCESS,"PAMI_Context_trylock");
+
+	return;
+}
+
+int PAMID_Progess_setup(pami_context_t context)
+{
+	pami_rc_t rc = PAMI_ERROR;
+
 
 	return PAMI_SUCCESS;
 }
 
 int PAMID_Progess_teardown(void)
 {
-  pami_result_t rc = PAMI_ERROR;
-  rc = PAMI_Extension_close(async_prog_ext);
-  PAMID_ASSERT(rc==PAMI_SUCCESS,"PAMI_Extension_close - EXT_async_progress");
 
-  return;
+	return;
 }
 
