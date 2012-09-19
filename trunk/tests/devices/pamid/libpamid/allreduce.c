c84b0982a8202547bc165bdf17b24d98a9c52e78
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2012-09-19 03:11:50 +0000

    adding more collectives
    git-svn-id: https://svn.mcs.anl.gov/repos/ospri@1103 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- trunk/tests/devices/pamid/libpamid/allreduce.c
+++ trunk/tests/devices/pamid/libpamid/allreduce.c
@@ -1,1 +1,72 @@
 #include "pamid.h"
+
+int PAMID_Allreduce_setup(pami_geometry_t geometry, pamid_collective_state_t * allreduce)
+{
+	pami_result_t rc = PAMI_ERROR;
+
+	allreduce->xfer = PAMI_XFER_ALLREDUCE;
+
+	/* query the geometry */
+	rc = PAMI_Geometry_algorithms_num(geometry, allreduce->xfer, allreduce->num_alg );
+	PAMID_ASSERT(rc==PAMI_SUCCESS,"PAMI_Geometry_algorithms_num");
+
+	allreduce->safe_algs = (pami_algorithm_t *) PAMIU_Malloc( allreduce->num_alg[0] * sizeof(pami_algorithm_t) );
+	allreduce->fast_algs = (pami_algorithm_t *) PAMIU_Malloc( allreduce->num_alg[1] * sizeof(pami_algorithm_t) );
+	allreduce->safe_meta = (pami_metadata_t  *) PAMIU_Malloc( allreduce->num_alg[0] * sizeof(pami_metadata_t)  );
+	allreduce->fast_meta = (pami_metadata_t  *) PAMIU_Malloc( allreduce->num_alg[1] * sizeof(pami_metadata_t)  );
+	rc = PAMI_Geometry_algorithms_query(geometry,
+			allreduce->xfer,
+			allreduce->safe_algs,
+			allreduce->safe_meta,
+			allreduce->num_alg[0],
+			allreduce->fast_algs,
+			allreduce->fast_meta,
+			allreduce->num_alg[1]);
+	PAMID_ASSERT(rc==PAMI_SUCCESS,"PAMI_Geometry_algorithms_query");
+
+	return PAMI_SUCCESS;
+}
+
+int PAMID_Allreduce_teardown(pamid_collective_state_t * allreduce)
+{
+	PAMIU_Free(allreduce->safe_algs);
+	PAMIU_Free(allreduce->fast_algs);
+	PAMIU_Free(allreduce->safe_meta);
+	PAMIU_Free(allreduce->fast_meta);
+
+	return PAMI_SUCCESS;
+}
+
+int PAMID_Allreduce_doit(pamid_collective_state_t * allreduce, int root, size_t num_bytes, void * buffer)
+{
+	pami_result_t rc = PAMI_ERROR;
+
+	size_t allreduce_alg = 0; /* 0 is not necessarily the best one... */
+
+	pami_xfer_t this;
+	volatile int active = 0;
+
+	this.cb_done   = cb_done;
+	this.cookie    = (void*) &active;
+	this.algorithm = allreduce->safe_algs[allreduce_alg]; /* safe algs should (must?) work */
+
+    allreduce.cmd.xfer_allreduce.op         = PAMI_DATA_SUM;
+    allreduce.cmd.xfer_allreduce.sndbuf     = (void*)sbuf;
+    allreduce.cmd.xfer_allreduce.stype      = PAMI_TYPE_SIGNED_INT;
+    allreduce.cmd.xfer_allreduce.stypecount = d;
+    allreduce.cmd.xfer_allreduce.rcvbuf     = (void*)rbuf;
+    allreduce.cmd.xfer_allreduce.rtype      = PAMI_TYPE_SIGNED_INT;
+    allreduce.cmd.xfer_allreduce.rtypecount = d;
+
+	/* perform a allreduce */
+	active = 1;
+	rc = PAMI_Collective(PAMID_INTERNAL_STATE.pami_contexts[0], &this );
+	PAMID_ASSERT(rc==PAMI_SUCCESS,"PAMI_Collective - allreduce");
+
+	while (active)
+		rc = PAMI_Context_trylock_advancev(PAMID_INTERNAL_STATE.pami_contexts[0], 1, 1000 );
+
+	PAMID_ASSERT(rc==PAMI_SUCCESS,"PAMI_Context_trylock_advancev - allreduce");
+
+	return PAMI_SUCCESS;
+}
