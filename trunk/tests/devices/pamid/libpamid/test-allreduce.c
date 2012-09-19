407310e505abf8e49aed928f3bf2f9116cb3b048
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2012-09-19 20:52:06 +0000

    allreduce test
    git-svn-id: https://svn.mcs.anl.gov/repos/ospri@1133 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- trunk/tests/devices/pamid/libpamid/test-allreduce.c
+++ trunk/tests/devices/pamid/libpamid/test-allreduce.c
@@ -4,7 +4,31 @@ int main(int argc, char * argv[])
 {
 	PAMID_Initialize();
 
-	PAMID_Barrier_world();
+	const int n = 1000;
+	int * a = malloc(n*sizeof(int));
+	int * b = malloc(n*sizeof(int));
+
+	int r = (int)PAMID_World_rank();
+	int s = (int)PAMID_World_size();
+
+	for (int i=0; i<n; i++)
+		a[i] = 1;
+
+	for (int i=0; i<n; i++)
+		b[i] = -1;
+
+	PAMID_Allreduce_world(n, a, b, PAMI_TYPE_SIGNED_INT, PAMI_DATA_SUM);
+
+	int errors = 0;
+	for (int i=0; i<n; i++)
+		if (b[i] != s)
+			errors++;
+
+	if (errors>0)
+		printf("rank %ld has %d errors \n", PAMID_World_rank(), errors );
+
+	free(b);
+	free(a);
 
 	PAMID_Finalize();
 
