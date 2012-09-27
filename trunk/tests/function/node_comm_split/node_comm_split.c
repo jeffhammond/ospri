6d76372ef9abca32929ccf30d3ee8f2e05a3b05d
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2012-09-27 17:38:42 +0000

    Pavan's better macro
    git-svn-id: https://svn.mcs.anl.gov/repos/ospri@1137 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- trunk/tests/function/node_comm_split/node_comm_split.c
+++ trunk/tests/function/node_comm_split/node_comm_split.c
@@ -6,20 +6,7 @@
 
 #include <mpi.h>
 
-#define NONPORTABLE
-//#define DEBUG
-
-#if defined(__bgp__) && defined(NONPORTABLE)
-#  include </bgsys/drivers/ppcfloor/arch/include/spi/kernel_interface.h>
-#  include </bgsys/drivers/ppcfloor/arch/include/common/bgp_personality.h>
-#  include </bgsys/drivers/ppcfloor/arch/include/common/bgp_personality_inlines.h>
-#endif
-
-#if defined(__bgq__) && defined(NONPORTABLE)
-//#  include <firmware/include/personality.h>
-//#  include <spi/include/kernel/process.h>
-#  include <spi/include/kernel/location.h>
-#endif
+#define DEBUG
 
 static inline int xstrcmp(const void *a, const void *b) 
 { 
@@ -28,59 +15,22 @@ static inline int xstrcmp(const void *a, const void *b)
     return strcmp(*ia, *ib);
 } 
 
-int main(int argc, char* argv[])
+int MPE_Comm_split_node(MPI_Comm * NodeComm)
 {
-    int mpi_result = MPI_SUCCESS;
-
-    mpi_result = MPI_Init(&argc,&argv);
-    assert(mpi_result==MPI_SUCCESS);
-
     int world_rank = -1, world_size = -1;
-    int node_rank = -1, node_size = -1;
 
-    mpi_result = MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-    assert(mpi_result==MPI_SUCCESS);
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-    mpi_result = MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-    assert(mpi_result==MPI_SUCCESS);
+#if MPI_VERSION >= 3
+    MPI_Comm_split_type(MPI_COMM_WORLD, MPI_COMM_TYPE_SHARED, 0, MPI_INFO_NULL, NodeComm);
+#elif defined (MPICH2) && (MPICH2_NUM_VERSION >= MPICH2_CALC_VERSION(1,5,0,0,0)) && 0
+    MPIX_Comm_split_type(MPI_COMM_WORLD, MPIX_COMM_TYPE_SHARED, 0, MPI_INFO_NULL, NodeComm);
+#else
 
     int namelen = 0;
     char procname[MPI_MAX_PROCESSOR_NAME];
-
-    MPI_Comm NodeComm;
-
-#if MPI_VERSION >= 3
-    mpi_result = MPI_Comm_split_type(MPI_COMM_WORLD, MPI_COMM_TYPE_SHARED, 0, MPI_INFO_NULL, &NodeComm);
-    assert(mpi_result==MPI_SUCCESS);
-#elif defined(MPICH2) && (MPICH2_NUMVERSION>10500000)
-    mpi_result = MPIX_Comm_split_type(MPI_COMM_WORLD, MPIX_COMM_TYPE_SHARED, 0, MPI_INFO_NULL, &NodeComm);
-    assert(mpi_result==MPI_SUCCESS);
-#else
-    int color = world_rank;
-# if defined(__bgq__) || defined(__bgp__) && defined(NONPORTABLE)
-#  warning nonportable
-    int ranks_per_node = 0;
-    int rank_in_node = -1;
-#  if defined(__bgq__)
-    rank_in_node = Kernel_MyTcoord();
-#  elif defined(__bgp__)
-    rank_in_node = Kernel_PhysicalProcessorID();
-#  endif
-    mpi_result = MPI_Allreduce( &rank_in_node, &ranks_per_node, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
-    assert(mpi_result==MPI_SUCCESS);
-    ranks_per_node++; /* change from [0,n-1] to [1,n] */
-    int my_node = (world_rank - rank_in_node)/ranks_per_node;
-
-#  ifdef DEBUG
-    int num_nodes = world_size/ranks_per_node;
-    printf("%7d: rank_in_node = %2d, ranks_per_node = %2d, my_node = %5d, num_nodes = %5d \n",
-            world_rank, rank_in_node, ranks_per_node, my_node, num_nodes);
-    fflush(stdout);
-#  endif
-    color = my_node;
-# else /* not Blue Gene */
     memset(procname, '\0', MPI_MAX_PROCESSOR_NAME);
-
     MPI_Get_processor_name( procname, &namelen );
 #  ifdef DEBUG
     printf("%d: processor name = %s gethostid = %ld \n" , world_rank, procname, gethostid() );
@@ -88,8 +38,7 @@ int main(int argc, char* argv[])
 #  endif
 
     int max_namelen = -1;
-    mpi_result = MPI_Allreduce( &namelen, &max_namelen, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
-    assert(mpi_result==MPI_SUCCESS);
+    MPI_Allreduce( &namelen, &max_namelen, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
     assert(max_namelen>0);
 
     char ** procname_array   = NULL;
@@ -111,8 +60,9 @@ int main(int argc, char* argv[])
             procname_array[i] = &procname_storage[i*max_namelen];
     }
 
-    mpi_result = MPI_Gather( procname, max_namelen, MPI_CHAR, procname_storage, max_namelen, MPI_CHAR, 0, MPI_COMM_WORLD );
-    assert(mpi_result==MPI_SUCCESS);
+    MPI_Gather( procname, max_namelen, MPI_CHAR, procname_storage, max_namelen, MPI_CHAR, 0, MPI_COMM_WORLD );
+
+    int color = world_rank;
 
     if (world_rank==0)
     {
@@ -132,14 +82,14 @@ int main(int argc, char* argv[])
         procname_colors = malloc( world_size * sizeof(int) );
         assert( procname_colors != NULL );
 
-        int color = 0;
+        color = 0;
         procname_colors[0] = color;
         for (i=1; i<world_size; i++)
         {
             if (0!=strncmp(procname_array[i], procname_array[i-1], max_namelen)) color++;
             procname_colors[i] = color;
 #  ifdef DEBUG
-            printf("procname_array[%d] = \n", i );
+            printf("procname_array[%d] = ", i );
             int j;
             for (j=0; j<max_namelen; j++) printf("%c", procname_array[i][j] );
             printf(" \n");
@@ -151,33 +101,42 @@ int main(int argc, char* argv[])
         free(procname_storage);
         free(procname_array);
     }
-    mpi_result = MPI_Scatter( procname_colors, 1, MPI_INT, &color, 1, MPI_INT, 0, MPI_COMM_WORLD );
-    assert(mpi_result==MPI_SUCCESS);
+    MPI_Scatter( procname_colors, 1, MPI_INT, &color, 1, MPI_INT, 0, MPI_COMM_WORLD );
 
     if (world_rank==0) 
         free(procname_colors);
 
-# endif /* if BG else MPI_Get_proc_name */
-    mpi_result = MPI_Comm_split(MPI_COMM_WORLD, color, 0, &NodeComm);
-    assert(mpi_result==MPI_SUCCESS);
+    MPI_Comm_split(MPI_COMM_WORLD, color, 0, NodeComm);
 #endif /* MPIX_Comm_split_type */
 
-    mpi_result = MPI_Get_processor_name( procname, &namelen );
-    assert(mpi_result==MPI_SUCCESS);
+    return MPI_SUCCESS;
+}
 
-    mpi_result = MPI_Comm_rank(NodeComm, &node_rank);
-    assert(mpi_result==MPI_SUCCESS);
+int main(int argc, char* argv[])
+{
+    MPI_Init(&argc,&argv);
 
-    mpi_result = MPI_Comm_size(NodeComm, &node_size);
-    assert(mpi_result==MPI_SUCCESS);
+    int world_rank = -1, world_size = -1;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    MPI_Comm NodeComm;
+    MPE_Comm_split_node(&NodeComm);
+
+    int node_rank = -1,  node_size = -1;
+    MPI_Comm_rank(NodeComm, &node_rank);
+    MPI_Comm_size(NodeComm, &node_size);
+
+    int namelen = 0;
+    char procname[MPI_MAX_PROCESSOR_NAME];
+    MPI_Get_processor_name( procname, &namelen );
 
     printf("%s: %d of %d on node, %d of %d on world \n", procname, node_rank, node_size, world_rank, world_size );
-    fflush(stdout);
 
-    mpi_result = MPI_Barrier(MPI_COMM_WORLD);
-    assert(mpi_result==MPI_SUCCESS);
+    MPI_Barrier(MPI_COMM_WORLD);
 
     if (world_rank==0) printf("%7d: all done! \n", world_rank );
+    fflush(stdout);
 
     MPI_Finalize();
 
