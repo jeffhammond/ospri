19e241d10a90ba2445302e199e00c88681c06b69
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2012-10-02 19:32:15 +0000

    add test of comm split only and some extra init checks
    git-svn-id: https://svn.mcs.anl.gov/repos/ospri@1145 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- trunk/tests/function/mpi/coll/comm_split.c
+++ trunk/tests/function/mpi/coll/comm_split.c
@@ -85,13 +85,6 @@ int main(int argc, char *argv[])
     if (world_rank==0) printf("MPI_Barrier on comm_world_dup \n");
     MPI_Barrier( comm_world_dup );
 
-    if (world_rank==0) printf("MPI_Comm_split of MPI_COMM_WORLD into odd-even \n");
-    MPI_Comm comm_world_oddeven;
-    MPI_Comm_split(MPI_COMM_WORLD, (int) (world_rank%2), world_rank, &comm_world_oddeven);
-
-    if (world_rank==0) printf("MPI_Barrier on comm_world_oddeven \n");
-    MPI_Barrier( comm_world_oddeven );
-
     if (world_rank==0) printf("MPI_Comm_split MPI_COMM_WORLD into (world-1) \n");
     MPI_Comm * comm_world_minus_ones = NULL;
     comm_world_minus_ones = (MPI_Comm *) safemalloc( world_size * sizeof(MPI_Comm) );
@@ -102,87 +95,12 @@ int main(int argc, char *argv[])
     for (int i=0; i<world_size; i++)
         MPI_Barrier( comm_world_minus_ones[i] );
 
-    if (world_rank==0) printf("MPI_Comm_group of group_world from MPI_COMM_WORLD \n");
-    MPI_Group group_world;
-    MPI_Comm_group(MPI_COMM_WORLD, &group_world);
-
-    int geomprog_size = (world_size==1) ? 1 : ceil(log2(world_size));
-
-    int * geomprog_list = NULL;
-    geomprog_list = (int *) safemalloc( geomprog_size * sizeof(int) );
-
-    for (int i=0; i<geomprog_size; i++)
-        geomprog_list[i] = pow(2,i)-1;
-
-#ifdef DEBUG
-    if (world_rank==0)
-        for (int i=0; i<geomprog_size; i++)
-            if (world_rank==0) printf("geomprog_list[%d] = %d \n", i, geomprog_list[i]);
-#endif
-
-    if (world_rank==0) printf("MPI_Group_incl of group_geomprog (geometric progression) from group_world \n");
-    MPI_Group group_geomprog;
-    MPI_Group_incl(group_world, geomprog_size, geomprog_list, &group_geomprog);
-
-    if (world_rank==0) printf("MPI_Comm_create of comm_geomprog from group_geomprog on MPI_COMM_WORLD \n");
-    MPI_Comm comm_geomprog;
-    MPI_Comm_create(MPI_COMM_WORLD, group_geomprog, &comm_geomprog);
-
-    if (world_rank==0) printf("MPI_Barrier on comm_geomprog \n");
-    for (int i=0; i<geomprog_size; i++)
-        if (geomprog_list[i]==world_rank)
-            MPI_Barrier( comm_geomprog );
-
-    if (world_rank==0) printf("MPI_Barrier on MPI_COMM_WORLD 2 \n");
-    MPI_Barrier( MPI_COMM_WORLD );
-
-    /*********************************************************************************
-     *                            COLLECTIVES
-     *********************************************************************************/
-
-    int max_mem = (argc>1 ? atoi(argv[1]) : 64*1024*1024);
-
-    bcast_only(stdout, MPI_COMM_WORLD, max_mem);
-    fflush(stdout);
-    MPI_Barrier( MPI_COMM_WORLD );
-
-#if 0
-    bcast_only(stdout, comm_world_dup, max_mem);
-    fflush(stdout);
-    MPI_Barrier( MPI_COMM_WORLD );
-
-    FILE * even_out = safefopen("./even.txt", "w+");
-    FILE * odd_out  = safefopen("./odd.txt", "w+");
-
-    if (world_rank%2==0)
-        bcast_only(even_out, comm_world_oddeven, max_mem);
-    if (world_rank%2==1)
-        bcast_only(odd_out,  comm_world_oddeven, max_mem);
-#endif
-
-    bcast_vs_scatter_allgather(stdout, MPI_COMM_WORLD, max_mem);
-    fflush(stdout);
-    MPI_Barrier( MPI_COMM_WORLD );
-    
-    allgather_only(stdout, MPI_COMM_WORLD, max_mem);
-    fflush(stdout);
-    MPI_Barrier( MPI_COMM_WORLD );
-
-    alltoall_only(stdout, MPI_COMM_WORLD, max_mem);
-    fflush(stdout);
-    MPI_Barrier( MPI_COMM_WORLD );
-
-    /*********************************************************************************
-     *                            CLEAN UP AND FINALIZE
-     *********************************************************************************/
-
     for (int i=0; i<world_size; i++)
         MPI_Comm_free(&comm_world_minus_ones[i]);
 
     MPI_Comm_free(&comm_world_dup);
 
     free(comm_world_minus_ones);
-    free(geomprog_list);
 
     MPI_Barrier( MPI_COMM_WORLD );
     MPI_Finalize();
