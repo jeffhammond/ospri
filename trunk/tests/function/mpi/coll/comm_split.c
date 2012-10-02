068f4c96fa19d9fc0ae80e74ff15b3d3927673f6
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2012-10-02 19:42:25 +0000

    git-svn-id: https://svn.mcs.anl.gov/repos/ospri@1146 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- trunk/tests/function/mpi/coll/comm_split.c
+++ trunk/tests/function/mpi/coll/comm_split.c
@@ -51,11 +51,13 @@ int main(int argc, char *argv[])
     {
         if (world_rank==0) printf("MPI_Init_thread returned %s instead of %s, but this is okay. \n",
                                   MPI_THREAD_STRING(provided), MPI_THREAD_STRING(requested) );
+        fflush(stdout);
     }
     if (provided<requested)
     {
         if (world_rank==0) printf("MPI_Init_thread returned %s instead of %s so the test will exit. \n",
                                   MPI_THREAD_STRING(provided), MPI_THREAD_STRING(requested) );
+        fflush(stdout);
         exit(1);
     }
 
@@ -64,34 +66,46 @@ int main(int argc, char *argv[])
     int is_init = 0;
     MPI_Initialized(&is_init);
     if (world_rank==0) printf("MPI %s initialized. \n", (is_init==1 ? "was" : "was not") );
+    fflush(stdout);
 
     MPI_Query_thread(&provided);
     if (world_rank==0) printf("MPI thread support is %s. \n", MPI_THREAD_STRING(provided) );
+    fflush(stdout);
 
     MPI_Comm_size( MPI_COMM_WORLD, &world_size );
     if (world_rank==0) printf("MPI test program running on %d ranks. \n", world_size);
+    fflush(stdout);
 
     /*********************************************************************************
      *                            SETUP MPI COMMUNICATORS
      *********************************************************************************/
 
     if (world_rank==0) printf("MPI_Barrier on MPI_COMM_WORLD 1 \n");
+    fflush(stdout);
     MPI_Barrier( MPI_COMM_WORLD );
 
     if (world_rank==0) printf("MPI_Comm_dup of MPI_COMM_WORLD \n");
+    fflush(stdout);
     MPI_Comm comm_world_dup;
     MPI_Comm_dup(MPI_COMM_WORLD, &comm_world_dup);
 
     if (world_rank==0) printf("MPI_Barrier on comm_world_dup \n");
+    fflush(stdout);
     MPI_Barrier( comm_world_dup );
 
     if (world_rank==0) printf("MPI_Comm_split MPI_COMM_WORLD into (world-1) \n");
+    fflush(stdout);
     MPI_Comm * comm_world_minus_ones = NULL;
     comm_world_minus_ones = (MPI_Comm *) safemalloc( world_size * sizeof(MPI_Comm) );
     for (int i=0; i<world_size; i++)
+    {
+        if (world_rank==0) printf("MPI_Comm_split number %d. \n", i);
+        fflush(stdout);
         MPI_Comm_split(MPI_COMM_WORLD, (int) (world_rank==i), world_rank, &comm_world_minus_ones[i]);
+    }
 
     if (world_rank==0) printf("MPI_Barrier on comm_world_minus_ones[] \n");
+    fflush(stdout);
     for (int i=0; i<world_size; i++)
         MPI_Barrier( comm_world_minus_ones[i] );
 
