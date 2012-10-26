b9e9a9bc1c8e014adc12c514237ab8c2570368b5
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2012-10-26 21:44:19 +0000

    check rc
    git-svn-id: https://svn.mcs.anl.gov/repos/ospri@1205 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- trunk/tests/devices/pamid/libpamid/cht.c
+++ trunk/tests/devices/pamid/libpamid/cht.c
@@ -8,15 +8,17 @@ void * PAMID_Progress_function(void * dummy)
 		while (1)
 		{
 			/* advance all contexts except the local blocking one */
-			PAMI_Context_trylock_advancev( &(PAMID_INTERNAL_STATE.pami_contexts[PAMID_INTERNAL_STATE.context_roles.local_offload_context]),
+			rc = PAMI_Context_trylock_advancev( &(PAMID_INTERNAL_STATE.pami_contexts[PAMID_INTERNAL_STATE.context_roles.local_offload_context]),
 					PAMID_INTERNAL_STATE.num_contexts-1, 1000 );
+			PAMID_ASSERT(rc==PAMI_SUCCESS,"PAMI_Context_trylock_advancev - CHT");
 		}
 	else
 		while (1)
 		{
 			/* only one context exists, we must advance it */
-			PAMI_Context_trylock_advancev( &(PAMID_INTERNAL_STATE.pami_contexts[PAMID_INTERNAL_STATE.context_roles.local_blocking_context]),
+			rc = PAMI_Context_trylock_advancev( &(PAMID_INTERNAL_STATE.pami_contexts[PAMID_INTERNAL_STATE.context_roles.local_blocking_context]),
 					1, 1000 );
+			PAMID_ASSERT(rc==PAMI_SUCCESS,"PAMI_Context_trylock_advancev - CHT");
 		}
 
 	return NULL;
