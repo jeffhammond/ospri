88a32b31a76ff0e3cc8f8fc1dda8bc29484b997a
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2012-11-01 06:00:13 +0000

    fix dumb error
    git-svn-id: https://svn.mcs.anl.gov/repos/ospri@1249 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- trunk/tests/devices/pamid/p2p/safemalloc.c
+++ trunk/tests/devices/pamid/p2p/safemalloc.c
@@ -8,12 +8,12 @@ void * safemalloc(size_t n)
     int rc;
     void * ptr;
     rc = posix_memalign( &ptr , ALIGNMENT , n );
+#endif
 
     if ( ptr == NULL || n<0 )
     {
         fprintf( stderr , "%ld bytes could not be allocated \n" , (long)n );
         exit(n);
     }
-#endif
     return ptr;
 }
