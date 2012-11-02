8fb36fb813ddf87078958933d938dacdd6512a85
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2012-11-02 18:29:26 +0000

    add check for allocation of 0 or less bytes
    git-svn-id: https://svn.mcs.anl.gov/repos/ospri@1265 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- trunk/tests/devices/pamid/p2p/safemalloc.c
+++ trunk/tests/devices/pamid/p2p/safemalloc.c
@@ -1,9 +1,19 @@
 #include "safemalloc.h"
 
+#define NO_MEMALIGN
+
 void * safemalloc(size_t n) 
 {
     int rc = 0;
     void * ptr = NULL;
+
+    if (n<=0)
+    {
+        fprintf( stderr , "attempting to allocate %ld bytes \n" , (long)n );
+        fflush(stderr);
+        sleep(1);
+        exit(11);
+    }
 
 #ifdef NO_MEMALIGN
     ptr = malloc( n );
