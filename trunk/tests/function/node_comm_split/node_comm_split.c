4c508213000bb3c04e362c54b483b4cb418863a4
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2012-12-20 20:07:29 +0000

    git-svn-id: https://svn.mcs.anl.gov/repos/ospri@1312 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- trunk/tests/function/node_comm_split/node_comm_split.c
+++ trunk/tests/function/node_comm_split/node_comm_split.c
@@ -15,25 +15,22 @@ static inline int xstrcmp(const void *a, const void *b)
     return strcmp(*ia, *ib);
 } 
 
-int MPE_Comm_split_node(MPI_Comm * NodeComm)
+int MPE_Comm_split_node(MPI_Comm ParentComm, MPI_Comm * NodeComm)
 {
     int world_rank = -1, world_size = -1;
 
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    MPI_Comm_rank(ParentComm, &world_rank);
+    MPI_Comm_size(ParentComm, &world_size);
 
     int namelen = 0;
     char procname[MPI_MAX_PROCESSOR_NAME];
     memset(procname, '\0', MPI_MAX_PROCESSOR_NAME);
     MPI_Get_processor_name( procname, &namelen );
-#  ifdef DEBUG
+
+#ifdef DEBUG
     printf("%d: processor name = %s gethostid = %ld \n" , world_rank, procname, gethostid() );
     fflush(stdout);
-#  endif
-
-    int max_namelen = -1;
-    MPI_Allreduce( &namelen, &max_namelen, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
-    assert(max_namelen>0);
+#endif
 
     char ** procname_array   = NULL;
     char *  procname_storage = NULL;
@@ -44,33 +41,33 @@ int MPE_Comm_split_node(MPI_Comm * NodeComm)
         procname_array = malloc( world_size * sizeof(char *) );
         assert( procname_array != NULL );
 
-        procname_storage = malloc( world_size * max_namelen * sizeof(char) );
+        procname_storage = malloc( world_size * namelen * sizeof(char) );
         assert( procname_storage != NULL );
 
-        memset( procname_storage, '\0', world_size * max_namelen);
+        memset( procname_storage, '\0', world_size * namelen);
 
         int i;
         for (i=0; i<world_size; i++)
-            procname_array[i] = &procname_storage[i*max_namelen];
+            procname_array[i] = &procname_storage[i*namelen];
     }
 
-    MPI_Gather( procname, max_namelen, MPI_CHAR, procname_storage, max_namelen, MPI_CHAR, 0, MPI_COMM_WORLD );
+    MPI_Gather( procname, namelen, MPI_CHAR, procname_storage, namelen, MPI_CHAR, 0, ParentComm );
 
     int color = world_rank;
 
     if (world_rank==0)
     {
         int i;
-#  ifdef DEBUG
+#ifdef DEBUG
         for (i=0; i<world_size; i++)
         {
             printf("node %d is ", i);
             int j;
-            for (j=0; j<max_namelen; j++) printf("%c", procname_array[i][j] );
+            for (j=0; j<namelen; j++) printf("%c", procname_array[i][j] );
             printf(" in procname_array\n");
         }
         fflush(stdout);
-#  endif
+#endif
         qsort(procname_array, world_size, sizeof(char *), (void*) &xstrcmp);
 
         procname_colors = malloc( world_size * sizeof(int) );
@@ -80,27 +77,27 @@ int MPE_Comm_split_node(MPI_Comm * NodeComm)
         procname_colors[0] = color;
         for (i=1; i<world_size; i++)
         {
-            if (0!=strncmp(procname_array[i], procname_array[i-1], max_namelen)) color++;
+            if (0!=strncmp(procname_array[i], procname_array[i-1], namelen)) color++;
             procname_colors[i] = color;
-#  ifdef DEBUG
+#ifdef DEBUG
             printf("procname_array[%d] = ", i );
             int j;
-            for (j=0; j<max_namelen; j++) printf("%c", procname_array[i][j] );
+            for (j=0; j<namelen; j++) printf("%c", procname_array[i][j] );
             printf(" \n");
-            printf("strcmp(procname_array[%d], procname_array[%d])) = %d \n", i, i-1, strncmp( procname_array[i], procname_array[i-1], max_namelen ) );
+            printf("strcmp(procname_array[%d], procname_array[%d])) = %d \n", i, i-1, strncmp( procname_array[i], procname_array[i-1], namelen ) );
             printf("procname_colors[%d] = %d \n", i, procname_colors[i] );
-#  endif
+#endif
         }
 
         free(procname_storage);
         free(procname_array);
     }
-    MPI_Scatter( procname_colors, 1, MPI_INT, &color, 1, MPI_INT, 0, MPI_COMM_WORLD );
+    MPI_Scatter( procname_colors, 1, MPI_INT, &color, 1, MPI_INT, 0, ParentComm );
 
     if (world_rank==0) 
         free(procname_colors);
 
-    MPI_Comm_split(MPI_COMM_WORLD, color, 0, NodeComm);
+    MPI_Comm_split(ParentComm, color, 0, NodeComm);
 
     return MPI_SUCCESS;
 }
@@ -119,7 +116,7 @@ int main(int argc, char* argv[])
 #elif defined (MPICH2) && (MPICH2_NUM_VERSION >= MPICH2_CALC_VERSION(1,5,0,0,0)) && 0
     MPIX_Comm_split_type(MPI_COMM_WORLD, MPIX_COMM_TYPE_SHARED, 0, MPI_INFO_NULL, &NodeComm);
 #else
-    MPE_Comm_split_node(&NodeComm);
+    MPE_Comm_split_node(MPI_COMM_WORLD, &NodeComm);
 #endif
 
     int node_rank = -1,  node_size = -1;
