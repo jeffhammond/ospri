f76624a0dae2c085eeebb2e4dbfcfd173ceed2d4
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2012-12-27 03:22:42 +0000

    need void in decl
    git-svn-id: https://svn.mcs.anl.gov/repos/ospri@1317 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- branches/scafacos/armci.h
+++ branches/scafacos/armci.h
@@ -1,15 +1,15 @@
-int ARMCI_Boot();
-int ARMCI_Init();
-void ARMCI_Finalize();
-void ARMCI_Cleanup();
+int ARMCI_Boot(void);
+int ARMCI_Init(void);
+void ARMCI_Finalize(void);
+void ARMCI_Cleanup(void);
 void ARMCI_Error(char *msg, int code);
 
 int ARMCI_Malloc(void * ptr_arr[], int bytes);
 int ARMCI_Free(void * ptr);
 
-void ARMCI_Barrier();
+void ARMCI_Barrier(void);
 
 void ARMCI_Fence(int proc);
-void ARMCI_AllFence();
+void ARMCI_AllFence(void);
 
 int ARMCI_Put(void *src, void *dst, int bytes, int proc);
