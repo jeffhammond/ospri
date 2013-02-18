50bf59e944b02f050bd444919b8063263cdbf0cf
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2013-02-18 17:57:08 +0000

    rename
    git-svn-id: https://svn.mcs.anl.gov/repos/ospri@1371 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- trunk/tests/devices/dmapp/cray_proc.c
+++ trunk/tests/devices/dmapp/cray_proc.c
@@ -13,20 +13,32 @@ int main(int argc, char * argv[])
   int rank, size;
 
   MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
 
   int verbose = ( argc > 1 ? atoi(argv[1]) : 0 );
 
-  FILE * pFile;
-  pFile = fopen ("myfile.txt","w");
-  if (pFile!=NULL)
-  {
-    fputs ("fopen example",pFile);
-    fclose (pFile);
+  FILE * procfile = fopen("/proc/cray_xt/cname","r");
+  if (procfile!=NULL) {
+
+    /* format example: c1-0c1s2n1 c3-0c2s15n3 */
+
+    char str[255];
+    fscanf(procfile, "%s", str);
+
+    rewind(procfile);
+    char a, b, c, d;
+    int i, j, k, l, m;
+    fscanf(procfile, "%c%d-%d%c%d%c%d%c%d", &a, &i, &j, &b, &k, &c, &l, &d, &m);
+
+    printf("%d: /proc/cray_xt/cname = %s \n", rank, str);
+    printf("%d: coords = (%d,%d,%d,%d,%d) \n", rank, i, j, k, l, m);
+
+    fclose(procfile);
+  } else {
+    fprintf(stderr, "fopen has failed! \n");
+    exit(1);
   }
-
-
-
-  fflush(stdout);
 
   MPI_Finalize();
 
