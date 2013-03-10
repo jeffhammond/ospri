834228bf2bd0afb03003faf88fb2fc3f603f6bcb
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2013-03-10 22:00:52 +0000

    demonstrate value of MPI_Receive_reduce for the MPI Forum
    git-svn-id: https://svn.mcs.anl.gov/repos/ospri@1402 4a0c8edc-5189-0410-a5da-cd986011a6a8

---
+++ trunk/tests/devices/pamid/receive_reduce/preamble.h
@@ -0,0 +1,54 @@
+#ifndef PREAMBLE_H
+#define PREAMBLE_H
+
+#include <pami.h>
+
+#define PRINT_SUCCESS 0
+
+#ifdef DEBUG
+#define TEST_ASSERT(c,m) \
+        do { \
+        if (!(c)) { \
+                    printf(m" FAILED on rank %ld\n", world_rank); \
+                    fflush(stdout); \
+                    sleep(1); \
+                    abort(); \
+                  } \
+        else if (PRINT_SUCCESS) { \
+                    printf(m" SUCCEEDED on rank %ld\n", world_rank); \
+                    fflush(stdout); \
+                  } \
+        } \
+        while(0);
+#else
+#define TEST_ASSERT(c,m) 
+#endif
+
+static size_t world_size, world_rank = -1;
+
+static void cb_done (void * ctxt, void * clientdata, pami_result_t err)
+{
+  int * active = (int *) clientdata;
+  (*active)--;
+}
+
+typedef struct done_s
+{
+    int local;
+    int remote;
+} 
+done_t;
+
+static void cb_done_local(void * ctxt, void * clientdata, pami_result_t err)
+{
+    done_t * temp = (done_t *) clientdata;
+    (temp->local)--;
+}
+
+static void cb_done_remote(void * ctxt, void * clientdata, pami_result_t err)
+{
+    done_t * temp = (done_t *) clientdata;
+    (temp->remote)--;
+}
+
+#endif
