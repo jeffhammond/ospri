54b86107055001f0ccd1addfcfe3eeb0278100d6
jhammond <jhammond@4a0c8edc-5189-0410-a5da-cd986011a6a8>
2013-04-11 03:15:05 +0000

    more printing
    git-svn-id: https://svn.mcs.anl.gov/repos/ospri@1440 4a0c8edc-5189-0410-a5da-cd986011a6a8

--- trunk/tests/devices/mpi-pt/hello.c
+++ trunk/tests/devices/mpi-pt/hello.c
@@ -8,8 +8,10 @@ int main(int argc, char * argv[])
     int provided;
 
     MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
-    if (provided!=MPI_THREAD_MULTIPLE) 
+    if (provided!=MPI_THREAD_MULTIPLE) {
+        printf("provided!=MPI_THREAD_MULTIPLE \n");
         MPI_Abort(MPI_COMM_WORLD, 1);
+    }
 
     int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
